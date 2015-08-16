f7ff6dc4d49cb78fd5d135d092d5f19410dd8f44
Jon Mason <jonmason@broadcom.com>
2015-08-16 17:51:42 -0400

    LINUXDEV-139: Correct NSP license and cleanup
    NSP: Correct NSP license and cleanup
    
    bcm_nsp.c had the incorrect license information.  This patch matches the
    license information to the one located in bcm_cygnus.c.
    
    Also, clean-up a minor whitespace issue (spaces instead of tabs).
    
    Bug: LINUXDEV-139
    Change-Id: Icc93d0c802c3498eed122d23e7f2f82832d91418
    Signed-off-by: Jon Mason <jonmason@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/7440
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Reviewed-by: Scott Branden <sbranden@broadcom.com>

--- arch/arm/mach-bcm/bcm_nsp.c
+++ arch/arm/mach-bcm/bcm_nsp.c
@@ -1,17 +1,14 @@
 /*
- * Copyright (C) 2015, Broadcom Corporation. All Rights Reserved.
+ * Copyright (C) 2015 Broadcom Corporation
  *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #include <asm/mach/arch.h>
@@ -22,7 +19,7 @@ static const char *const bcm_nsp_dt_compat[] __initconst = {
 };
 
 DT_MACHINE_START(NSP_DT, "Broadcom Northstar Plus SoC")
-	.l2c_aux_val    = 0,
-	.l2c_aux_mask   = ~0,
+	.l2c_aux_val	= 0,
+	.l2c_aux_mask	= ~0,
 	.dt_compat = bcm_nsp_dt_compat,
 MACHINE_END
