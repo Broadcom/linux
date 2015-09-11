52cf1cc40d83c00fbdd2ce7380b7f873e95fed39
Anup Patel <anup.patel@broadcom.com>
2015-09-11 13:49:39 +0530

    CTRL-20951: Update iproc_defconfig for ARM SP805 watchdog driver
    This patch enables ARM SP805 watchdog driver in iproc_defconfig
    and BRCM defconfig generation tool.
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Bug: CTRL-20951
    Change-Id: Ia18ec109c7c0456bdf7e1a663e977ab808fcff4f
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/8673
    Reviewed-by: Ray Jui <rjui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Reviewed-by: Vikram Prakash <vikramp@broadcom.com>
    Reviewed-by: Scott Branden <sbranden@broadcom.com>

--- brcm-genconfigs/update_defconfig.sh
+++ brcm-genconfigs/update_defconfig.sh
@@ -75,7 +75,7 @@ main() {
 	fi
 
 	# iproc_defconfig, for both NS2 and Stingray
-	do_update iproc base-arm64 clocksource dbg i2c mmc
+	do_update iproc base-arm64 clocksource dbg i2c mmc watchdog
 
 	# add support for other defconfigs here
 
