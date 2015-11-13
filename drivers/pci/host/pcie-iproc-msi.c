58d5579a6479d9774927d0659c18e86ddca3f080
Ray Jui <rjui@broadcom.com>
2015-11-12 16:59:38 -0800

    CTRL-20881: Improve iProc MSI register mapping
    PCI: iproc: Improve iProc MSI register mapping logic
    
    Improve the iProc PCIe MSI driver register mapping logic by
    making more generic
    
    Signed-off-by: Ray Jui <rjui@broadcom.com>
    
    Bug: CTRL-20881
    Change-Id: Id5b461554189d0938e2235d1cc6b9b1b777ab323
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/11964
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <rjui@broadcom.com>
    Tested-by: Ray Jui <rjui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Reviewed-by: Scott Branden <sbranden@broadcom.com>

--- drivers/pci/host/pcie-iproc-msi.c
+++ drivers/pci/host/pcie-iproc-msi.c
@@ -25,24 +25,35 @@
 
 #include "pcie-iproc.h"
 
-#define SYS_MSI_INTS_EN_OFFSET       0x208
-#define SYS_MSI_INTR_EN_SHIFT        11
-#define SYS_MSI_INTR_EN              BIT(SYS_MSI_INTR_EN_SHIFT)
-#define SYS_MSI_INT_N_EVENT_SHIFT    1
-#define SYS_MSI_INT_N_EVENT          BIT(SYS_MSI_INT_N_EVENT_SHIFT)
-#define SYS_MSI_EQ_EN_SHIFT          0
-#define SYS_MSI_EQ_EN                BIT(SYS_MSI_EQ_EN_SHIFT)
+#define IPROC_MSI_INTS_EN_OFFSET       0x208
+#define IPROC_MSI_INTR_EN_SHIFT        11
+#define IPROC_MSI_INTR_EN              BIT(IPROC_MSI_INTR_EN_SHIFT)
+#define IPROC_MSI_INT_N_EVENT_SHIFT    1
+#define IPROC_MSI_INT_N_EVENT          BIT(IPROC_MSI_INT_N_EVENT_SHIFT)
+#define IPROC_MSI_EQ_EN_SHIFT          0
+#define IPROC_MSI_EQ_EN                BIT(IPROC_MSI_EQ_EN_SHIFT)
 
-#define SYS_EQ_MASK                  0x3f
+#define IPROC_MSI_EQ_MASK              0x3f
 
 /* number of queues in each event queue */
-#define SYS_EQ_LEN                   64
+#define IPROC_MSI_EQ_LEN               64
 
 /* size of each event queue memory region */
 #define EQ_MEM_REGION_SIZE           SZ_4K
 
 /* size of each MSI message memory region */
 #define MSI_MSG_MEM_REGION_SIZE      SZ_4K
+
+enum iproc_msi_reg {
+	IPROC_MSI_EQ_PAGE = 0,
+	IPROC_MSI_EQ_PAGE_UPPER,
+	IPROC_MSI_PAGE,
+	IPROC_MSI_PAGE_UPPER,
+	IPROC_MSI_CTRL,
+	IPROC_MSI_EQ_HEAD,
+	IPROC_MSI_EQ_TAIL,
+	IPROC_MSI_REG_SIZE,
+};
 
 /**
  * iProc event queue based MSI
@@ -52,6 +63,7 @@
  *
  * @pcie: pointer to iProc PCIe data
  * @chip: MSI controller
+ * @reg_offsets: MSI register offsets
  * @irqs: pointer to an array that contains the interrupt IDs
  * @nirqs: number of total interrupts
  * @has_inten_reg: indicates the MSI interrupt enable register needs to be
@@ -69,6 +81,7 @@
 struct iproc_msi {
 	struct iproc_pcie *pcie;
 	struct msi_controller chip;
+	const u16 (*reg_offsets)[IPROC_MSI_REG_SIZE];
 	int *irqs;
 	int nirqs;
 	bool has_inten_reg;
@@ -81,47 +94,45 @@ struct iproc_msi {
 	void *msi_base;
 };
 
-/**
- * Required to accommodate different register layout between PAXB and PAXC
- */
-struct iproc_msi_reg {
-	u32 eq_page_offset;
-	u32 eq_page_upper_offset;
-	u32 msi_page_offset;
-	u32 msi_page_upper_offset;
-	u32 msi_ctrl_offset;
-	u32 eq_head_offset;
-	u32 eq_tail_offset;
+static const u16
+iproc_msi_reg_paxb[IPROC_PCIE_MAX_NUM_IRQS][IPROC_MSI_REG_SIZE] = {
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x210, 0x250, 0x254 },
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x214, 0x258, 0x25c },
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x218, 0x260, 0x264 },
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x21c, 0x268, 0x26c },
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x220, 0x270, 0x274 },
+	{ 0x200, 0x2c0, 0x204, 0x2c4, 0x224, 0x278, 0x27c },
 };
 
-static const struct iproc_msi_reg iproc_msi_reg[][IPROC_PCIE_MAX_NUM_IRQS] = {
-	[IPROC_PCIE_PAXB] = {
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x210, 0x250, 0x254 },
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x214, 0x258, 0x25c },
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x218, 0x260, 0x264 },
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x21c, 0x268, 0x26c },
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x220, 0x270, 0x274 },
-		{ 0x200, 0x2c0, 0x204, 0x2c4, 0x224, 0x278, 0x27c },
-	},
-	[IPROC_PCIE_PAXC] = {
-		{ 0xc00, 0xc04, 0xc08, 0xc0c, 0xc40, 0xc50, 0xc60 },
-		{ 0xc10, 0xc14, 0xc18, 0xc1c, 0xc44, 0xc54, 0xc64 },
-		{ 0xc20, 0xc24, 0xc28, 0xc2c, 0xc48, 0xc58, 0xc68 },
-		{ 0xc30, 0xc34, 0xc38, 0xc3c, 0xc4c, 0xc5c, 0xc6c },
-	},
+static const u16
+iproc_msi_reg_paxc[IPROC_PCIE_MAX_NUM_IRQS][IPROC_MSI_REG_SIZE] = {
+	{ 0xc00, 0xc04, 0xc08, 0xc0c, 0xc40, 0xc50, 0xc60 },
+	{ 0xc10, 0xc14, 0xc18, 0xc1c, 0xc44, 0xc54, 0xc64 },
+	{ 0xc20, 0xc24, 0xc28, 0xc2c, 0xc48, 0xc58, 0xc68 },
+	{ 0xc30, 0xc34, 0xc38, 0xc3c, 0xc4c, 0xc5c, 0xc6c },
 };
-
-#define EQ_PAGE(type, eq) (iproc_msi_reg[type][eq].eq_page_offset)
-#define EQ_PAGE_UPPER(type, eq) (iproc_msi_reg[type][eq].eq_page_upper_offset)
-#define MSI_PAGE(type, eq) (iproc_msi_reg[type][eq].msi_page_offset)
-#define MSI_PAGE_UPPER(type, eq) (iproc_msi_reg[type][eq].msi_page_upper_offset)
-#define MSI_CTRL(type, eq) (iproc_msi_reg[type][eq].msi_ctrl_offset)
-#define EQ_HEAD(type, eq) (iproc_msi_reg[type][eq].eq_head_offset)
-#define EQ_TAIL(type, eq) (iproc_msi_reg[type][eq].eq_tail_offset)
 
 static inline struct iproc_msi *to_iproc_msi(struct msi_controller *chip)
 {
 	return container_of(chip, struct iproc_msi, chip);
+}
+
+static inline u32 iproc_msi_read_reg(struct iproc_msi *msi,
+				     enum iproc_msi_reg reg,
+				     unsigned int eq)
+{
+	struct iproc_pcie *pcie = msi->pcie;
+
+	return readl(pcie->base + msi->reg_offsets[eq][reg]);
+}
+
+static inline void iproc_msi_write_reg(struct iproc_msi *msi,
+				       enum iproc_msi_reg reg,
+				       int eq, u32 val)
+{
+	struct iproc_pcie *pcie = msi->pcie;
+
+	writel(val, pcie->base + msi->reg_offsets[eq][reg]);
 }
 
 static int iproc_msi_alloc(struct iproc_msi *msi)
@@ -232,14 +243,14 @@ static void iproc_msi_enable(struct iproc_msi *msi)
 		phys_addr_t addr =
 			virt_to_phys(msi->eq_base + (i * EQ_MEM_REGION_SIZE));
 
-		writel(lower_32_bits(addr),
-		       pcie->base + EQ_PAGE(pcie->type, i));
+		iproc_msi_write_reg(msi, IPROC_MSI_EQ_PAGE, i,
+				    lower_32_bits(addr));
 
 		if (IS_ENABLED(CONFIG_PHYS_ADDR_T_64BIT))
 			val = upper_32_bits(addr);
 		else
 			val = 0;
-		writel(val, pcie->base + EQ_PAGE_UPPER(pcie->type, i));
+		iproc_msi_write_reg(msi, IPROC_MSI_EQ_PAGE_UPPER, i, val);
 	}
 
 	/* program memory region for MSI posted writes */
@@ -248,29 +259,30 @@ static void iproc_msi_enable(struct iproc_msi *msi)
 			virt_to_phys(msi->msi_base +
 				     (i * MSI_MSG_MEM_REGION_SIZE));
 
-		writel(lower_32_bits(addr),
-		       pcie->base + MSI_PAGE(pcie->type, i));
+		iproc_msi_write_reg(msi, IPROC_MSI_PAGE, i,
+				    lower_32_bits(addr));
 
 		if (IS_ENABLED(CONFIG_PHYS_ADDR_T_64BIT))
 			val = upper_32_bits(addr);
 		else
 			val = 0;
-		writel(val, pcie->base + MSI_PAGE_UPPER(pcie->type, i));
+		iproc_msi_write_reg(msi, IPROC_MSI_PAGE_UPPER, i, val);
 	}
 
 	for (eq = 0; eq < msi->nirqs; eq++) {
 		/* enable MSI event queue */
-		val = SYS_MSI_INTR_EN | SYS_MSI_INT_N_EVENT | SYS_MSI_EQ_EN;
-		writel(val, pcie->base + MSI_CTRL(pcie->type, eq));
+		val = IPROC_MSI_INTR_EN | IPROC_MSI_INT_N_EVENT |
+			IPROC_MSI_EQ_EN;
+		iproc_msi_write_reg(msi, IPROC_MSI_CTRL, eq, val);
 
 		/*
 		 * Some legacy platforms require the MSI interrupt enable
 		 * register to be set explicitly
 		 */
 		if (msi->has_inten_reg) {
-			val = readl(pcie->base + SYS_MSI_INTS_EN_OFFSET);
+			val = readl(pcie->base + IPROC_MSI_INTS_EN_OFFSET);
 			val |= BIT(eq);
-			writel(val, pcie->base + SYS_MSI_INTS_EN_OFFSET);
+			writel(val, pcie->base + IPROC_MSI_INTS_EN_OFFSET);
 		}
 	}
 }
@@ -291,21 +303,22 @@ static void iproc_msi_handler(struct irq_desc *desc)
 
 	eq = irq - msi->irqs[0];
 	virq = irq_find_mapping(msi->domain, eq);
-	head = readl(pcie->base + EQ_HEAD(pcie->type, eq)) & SYS_EQ_MASK;
+	head = iproc_msi_read_reg(msi, IPROC_MSI_EQ_HEAD, eq) &
+		IPROC_MSI_EQ_MASK;
 	do {
-		tail = readl(pcie->base + EQ_TAIL(pcie->type, eq)) &
-			SYS_EQ_MASK;
+		tail = iproc_msi_read_reg(msi, IPROC_MSI_EQ_TAIL, eq) &
+			IPROC_MSI_EQ_MASK;
 
 		num_events = (tail < head) ?
-			(SYS_EQ_LEN - (head - tail)) : (tail - head);
+			(IPROC_MSI_EQ_LEN - (head - tail)) : (tail - head);
 		if (!num_events)
 			break;
 
 		generic_handle_irq(virq);
 
 		head++;
-		head %= SYS_EQ_LEN;
-		writel(head, pcie->base + EQ_HEAD(pcie->type, eq));
+		head %= IPROC_MSI_EQ_LEN;
+		iproc_msi_write_reg(msi, IPROC_MSI_EQ_HEAD, eq, head);
 	} while (true);
 
 	chained_irq_exit(irq_chip, desc);
@@ -325,8 +338,19 @@ struct msi_controller *iproc_pcie_msi_init(struct iproc_pcie *pcie,
 	if (!msi)
 		return ERR_PTR(-ENOMEM);
 	msi->pcie = pcie;
-
 	mutex_init(&msi->bitmap_lock);
+
+	switch (pcie->type) {
+	case IPROC_PCIE_PAXB:
+		msi->reg_offsets = iproc_msi_reg_paxb;
+		break;
+	case IPROC_PCIE_PAXC:
+		msi->reg_offsets = iproc_msi_reg_paxc;
+		break;
+	default:
+		dev_err(pcie->dev, "incompatible iProc PCIe interface\n");
+		return ERR_PTR(-EINVAL);
+	}
 
 	ret = of_property_read_u32(node, "brcm,num-eq-region",
 				   &msi->n_eq_region);
