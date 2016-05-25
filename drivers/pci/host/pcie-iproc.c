d8c021451c0b6092f30987b663190ca6bfbe0856
Ray Jui <ray.jui@broadcom.com>
2016-05-24 17:57:59 -0700

    CTRL-21959: Add quirk for Stingray PAXC
    PCI: iproc: Add quirk for Stingray PAXC and Nitro
    
    Add quirk for Stingray PAXC and Nitro that currently use a PCI device
    ID of 0x16ca. It's uncertain whether this device ID will be changed in
    the future but that's on the hand of the controller team
    
    DO NOT UPSTREAM!!!
    
    Signed-off-by: Ray Jui <ray.jui@broadcom.com>
    
    Bug: CTRL-21959
    Change-Id: If3c33e7ee521455a67bf5171557cd57a09b206fa
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/23181
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Darren Edamura <darren.edamura@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/pci/host/pcie-iproc.c
+++ drivers/pci/host/pcie-iproc.c
@@ -776,6 +776,7 @@ static void quirk_paxc_bridge(struct pci_dev *pdev)
 }
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_BROADCOM, PCI_DEVICE_ID_NX2_57810,
 			quirk_paxc_bridge);
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_BROADCOM, 0x16ca, quirk_paxc_bridge);
 DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_BROADCOM, 0x16cd, quirk_paxc_bridge);
 
 MODULE_AUTHOR("Ray Jui <rjui@broadcom.com>");
