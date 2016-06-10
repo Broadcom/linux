388aa083a58ec8c12eb14f771f58c7ddcc7077ac
Raveendra Padasalagi <raveendra.padasalagi@broadcom.com>
2016-06-10 12:42:23 +0530

    CTRL-20886:Add header file for SHA3 algorithm's
    Currently linux kernel doesn't have software implementation
    of SHA3 algorithm. There is one RFC implementing SHA3 but
    it's not merged to kernel yet. SPU2 engine in stingray supports
    SHA3 crypto offload functionality. To add support for SHA3 in
    SPU2 crypto driver we needed to add generic defines in a header
    file and include it in the driver. So adding this header file.
    
    Bug: CTRL-20886
    Change-Id: Ieb34caaa65222163e170fa8c7664e486abb06922
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/24112
    Tested-by: RAVEENDRA PADASALAGI <raveendra.padasalagi@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

---
+++ include/crypto/sha3.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation (the "GPL").
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 (GPLv2) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 (GPLv2) along with this source code.
+ */
+
+#ifndef __CRYPTO_SHA3_H__
+#define __CRYPTO_SHA3_H__
+
+#define SHA3_224_DIGEST_SIZE	(224 / 8)
+#define SHA3_224_BLOCK_SIZE	(200 - 2 * SHA3_224_DIGEST_SIZE)
+
+#define SHA3_256_DIGEST_SIZE	(256 / 8)
+#define SHA3_256_BLOCK_SIZE	(200 - 2 * SHA3_256_DIGEST_SIZE)
+
+#define SHA3_384_DIGEST_SIZE	(384 / 8)
+#define SHA3_384_BLOCK_SIZE	(200 - 2 * SHA3_384_DIGEST_SIZE)
+
+#define SHA3_512_DIGEST_SIZE	(512 / 8)
+#define SHA3_512_BLOCK_SIZE	(200 - 2 * SHA3_512_DIGEST_SIZE)
+
+#endif
