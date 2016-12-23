154548eeb1c537a2c2036dd1e3ecc54ea96f8724
ismail <ismail@broadcom.com>
2016-12-23 11:11:04 -0800

    VOIP-6635: Enable the L2C in the Cygnus platform init
    arm: mach-bcm:This change configures the secure L2C
    Aux control register using the standard ARM smccc call.
    
    This change requires the presence of OPTEE-OS to be running in
    the secure world. The OPTEE-OS handler will be trigged by the
    smccc call and the handler will call the L2C enable function.
    
    This change combined with the OPTEE-OS in secure world,
    will allow the Kernel to boot only in non-secure mode.
    
    Tested-by: ismail <ismail@broadcom.com>
    Signed-off-by: ismail <ismail@broadcom.com>
    
    Bug: VOIP-6635
    Change-Id: Ic22653b97af15de306f612cec1ca471ff41f34d6
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/34799
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: CCXSW <ccxswbuild@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Shamez Kurji <shamez.kurji@broadcom.com>

--- arch/arm/mach-bcm/bcm_cygnus.c
+++ arch/arm/mach-bcm/bcm_cygnus.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Broadcom Corporation
+ * Copyright (C) 2014-2017 Broadcom
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -10,8 +10,10 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-
 #include <asm/mach/arch.h>
+#include <asm/mach/bcm_optee_smc.h>
+#include <asm/mach/map.h>
+#include <asm/proc-fns.h>
 #include "cygnus_pm.h"
 
 static const char * const bcm_cygnus_dt_compat[] __initconst = {
@@ -19,7 +21,18 @@ static const char * const bcm_cygnus_dt_compat[] __initconst = {
 	NULL,
 };
 
+static void cygnus_early_init(void)
+{
+	struct arm_smccc_res res;
+	unsigned int cmd;
+
+	cmd = FAST_SMC(SSAPI_ENABLE_L2_CACHE);
+	arm_smccc_smc(cmd, machine_desc->l2c_aux_val,
+		      machine_desc->l2c_aux_mask, 0, 0, 0, 0, 0, &res);
+}
+
 DT_MACHINE_START(BCM_CYGNUS_DT, "Broadcom Cygnus SoC")
+	.init_early	= cygnus_early_init,
 	.init_late	= cygnus_pm_init,
 	.l2c_aux_val	= 0,
 	.l2c_aux_mask	= ~0,
