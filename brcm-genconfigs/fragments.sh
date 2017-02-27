d164e8c5597bcf2ee43776787b5b0055dbfe247d
Jonathan Richardson <jonathan.richardson@broadcom.com>
2017-02-27 14:07:49 -0800

    VOIP-6759: Add support for usb host and device
    - Renamed bdc to usbd for usb device.
    - Added USB_CONFIGFS and USB_CONFIGFS_ACM to usbd to enable gadget
      configfs configuration. configfs is required to properly configure usb
      gadget drivers for Cygnus on the bcm/master kernel due to a bug in the
      USB h/w.
    - Added ehci and ohci for usb 2.0 and 1.1 host support to usb.cfg
    
    Change-Id: I5595221606ce425cc2b53014d467901c06bd11f0
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/37604
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Jonathan Richardson <jonathan.richardson@broadcom.com>
    Tested-by: Jonathan Richardson <jonathan.richardson@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: CCXSW <ccxswbuild@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- brcm-genconfigs/fragments.sh
+++ brcm-genconfigs/fragments.sh
@@ -8,8 +8,9 @@ arm64_defconfigs="iproc_defconfig sr_nitro_lite_defconfig"
 # Defines the fragments that apply to each defconfig. Variable name must be the
 # same as the defconfig file.
 bcm_cygnus_defconfig="base-arm32 blk dbg dma fs i2c mailbox md misc mmc mtd \
-    net pwm rng spi watchdog bt wifi regulator cygnus lcd sound trace stmpe"
+    net pwm rng spi watchdog bt wifi regulator cygnus lcd sound trace stmpe \
+    usb usbd"
 iproc_defconfig="base-arm64 clocksource dbg mmu blk net dma fs i2c iomux \
-    mailbox md misc mmc mtd mtd-spi rng spi virtio watchdog pwm usb bdc perf \
+    mailbox md misc mmc mtd mtd-spi rng spi virtio watchdog pwm usb usbd perf \
     regulator rtc infiniband nvme uio crypto hba bpf"
 sr_nitro_lite_defconfig="base-arm64 clocksource dbg mmu blk fs net"
