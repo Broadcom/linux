3e93a7746942eac78fd536c0c88135554bbb8b20
Lori Hikichi <lhikichi@broadcom.com>
2017-03-10 16:45:57 -0800

    VOIP-5343: Add ASoC codec for BRCM generic test codec
    This driver is very similar to the "dummy" driver found in the ASoC
    framework, but we are allowing this driver to be continuous frame rate
    up to a maximum of 384 kHz.  It is meant to model a piece of hardware
    that may be attached to a port for such uses as testing and development
    End products should create or make use of a more specific codec driver.
    
    Bug: VOIP-5343
    Change-Id: Ieb8723ea29e8a21fcdd8c4ebf1b0901f25ae9622
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/38267
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Reviewed-by: CCXSW <ccxswbuild@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

---
+++ sound/soc/codecs/bcmdummy-codec.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2017 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation (the "GPL").
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 (GPLv2) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 (GPLv2) along with this source code.
+ */
+/*
+ * This driver is very similar to the "dummy" driver found in the ASoC
+ * framework, but we are allowing this driver to be continuous frame rate up to
+ * a maximum of 384 kHz.  It is meant to model a piece of hardware that may
+ * be attached to a port for such uses as testing and development.
+ */
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/of.h>
+#include <linux/slab.h>
+#include <sound/initval.h>
+#include <sound/soc.h>
+#include <sound/pcm.h>
+
+static struct snd_soc_codec_driver bcm_dummy_codec;
+
+#define DUMMY_FORMATS	(SNDRV_PCM_FMTBIT_S8 | \
+			SNDRV_PCM_FMTBIT_U8 | \
+			SNDRV_PCM_FMTBIT_S16_LE | \
+			SNDRV_PCM_FMTBIT_U16_LE | \
+			SNDRV_PCM_FMTBIT_S24_LE | \
+			SNDRV_PCM_FMTBIT_U24_LE | \
+			SNDRV_PCM_FMTBIT_S32_LE | \
+			SNDRV_PCM_FMTBIT_U32_LE | \
+			SNDRV_PCM_FMTBIT_IEC958_SUBFRAME_LE)
+
+static struct snd_soc_dai_driver bcm_dummy_dai = {
+	.name		= "bcm-dummy-codec-dai",
+	.playback	= {
+		.stream_name	= "Playback",
+		.channels_min	= 1,
+		.channels_max	= 384,
+		.rates		= SNDRV_PCM_RATE_CONTINUOUS,
+		.rate_min	= 0,
+		.rate_max	= 384000,
+		.formats	= DUMMY_FORMATS,
+	},
+	.capture	= {
+		.stream_name	= "Capture",
+		.channels_min	= 1,
+		.channels_max	= 384,
+		.rates		= SNDRV_PCM_RATE_CONTINUOUS,
+		.rate_min	= 0,
+		.rate_max	= 384000,
+		.formats	= DUMMY_FORMATS,
+	},
+};
+
+static int bcm_dummy_probe(struct platform_device *pdev)
+{
+	return snd_soc_register_codec(&pdev->dev, &bcm_dummy_codec,
+			&bcm_dummy_dai, 1);
+}
+
+static int bcm_dummy_remove(struct platform_device *pdev)
+{
+	snd_soc_unregister_codec(&pdev->dev);
+	return 0;
+}
+
+static const struct of_device_id bcm_dummy_of_match[] = {
+	{.compatible = "brcm,dummy_codec", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, bcm_dummy_of_match);
+
+static struct platform_driver bcm_dummy_codec_driver = {
+	.probe		= bcm_dummy_probe,
+	.remove		= bcm_dummy_remove,
+	.driver		= {
+		.name	= "bcm-dummy-codec",
+		.of_match_table = of_match_ptr(bcm_dummy_of_match),
+	},
+};
+
+module_platform_driver(bcm_dummy_codec_driver);
+
+MODULE_DESCRIPTION("BCM Dummy Codec driver");
+MODULE_AUTHOR("Broadcom");
+MODULE_LICENSE("GPL v2");
