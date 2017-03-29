f5793cc829a4c7f8cc806d07991b17316b916ade
Scott Branden <scott.branden@broadcom.com>
2017-03-29 10:37:04 -0700

    SOC-4556 add pci.cfg
    Move pci configurations to pci.cfg
    
    Change-Id: I4f5072cc04b0a1f2fec1d272f326d85b30f6e1cc
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/39124
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>

--- brcm-genconfigs/fragments.sh
+++ brcm-genconfigs/fragments.sh
@@ -12,5 +12,6 @@ bcm_cygnus_defconfig="base-arm32 blk dbg dma fs i2c mailbox misc mmc mtd \
     usb usbd input"
 iproc_defconfig="base-arm64 clocksource dbg mmu blk net dma fs i2c iomux \
     mailbox md misc mmc mtd mtd-spi rng spi virtio watchdog pwm usb usbd perf \
-    regulator rtc infiniband nvme uio crypto hba bpf vfio profiling input ata"
+    regulator rtc infiniband nvme uio crypto hba bpf vfio profiling input ata \
+    pci"
 sr_nitro_lite_defconfig="base-arm64 clocksource dbg mmu blk fs net"
