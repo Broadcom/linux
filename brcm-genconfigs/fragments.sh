44df8d4e98bf6f8aeeac789427b9757f8a7c74fc
Scott Branden <scott.branden@broadcom.com>
2017-03-29 10:43:34 -0700

    SOC-4556 move dte to dte.cfg
    Move dte to dte.cfg
    
    Change-Id: If7d09b68948613c59441e032fda3c2f6b7db3a8c
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/39126
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>

--- brcm-genconfigs/fragments.sh
+++ brcm-genconfigs/fragments.sh
@@ -7,11 +7,11 @@ arm64_defconfigs="iproc_defconfig sr_nitro_lite_defconfig"
 
 # Defines the fragments that apply to each defconfig. Variable name must be the
 # same as the defconfig file.
-bcm_cygnus_defconfig="base-arm32 blk dbg dma fs i2c mailbox misc mmc mtd \
+bcm_cygnus_defconfig="base-arm32 blk dbg dma dte fs i2c mailbox misc mmc mtd \
     net pwm rng spi watchdog bt wifi regulator cygnus lcd sound trace stmpe \
     usb usbd input"
 iproc_defconfig="base-arm64 clocksource dbg mmu blk net dma fs i2c iomux \
     mailbox md misc mmc mtd mtd-spi rng spi virtio watchdog pwm usb usbd perf \
     regulator rtc infiniband nvme uio crypto hba bpf vfio profiling input ata \
-    pci"
+    pci dte"
 sr_nitro_lite_defconfig="base-arm64 clocksource dbg mmu blk fs net"
