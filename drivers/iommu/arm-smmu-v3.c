74bba36fbc56ed60bb6c1f5d2b470dabd8dee153
Anup Patel <anup.patel@broadcom.com>
2017-07-17 15:36:19 +0530

    SOC-5582: add IOMMU_CAP_BYPASS to the ARM SMMUv3 driver
    iommu/arm-smmu-v3: add IOMMU_CAP_BYPASS to the ARM SMMUv3 driver
    
    The ARM SMMUv3 support bypassing transactions for which domain
    is not configured. The patch adds corresponding IOMMU capability
    to advertise this fact.
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Change-Id: Ic973923ed401b6cf6e3194c523815fbf1b7ab34f
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/44447
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: CCXSW <ccxswbuild@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/iommu/arm-smmu-v3.c
+++ drivers/iommu/arm-smmu-v3.c
@@ -1376,6 +1376,8 @@ static bool arm_smmu_capable(enum iommu_cap cap)
 		return true;
 	case IOMMU_CAP_NOEXEC:
 		return true;
+	case IOMMU_CAP_BYPASS:
+		return true;
 	default:
 		return false;
 	}
