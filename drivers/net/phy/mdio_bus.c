25fb929933ed389c70f3308dac3365d89fbd20c3
Abhishek Shah <abhishek.shah@broadcom.com>
2017-08-21 18:49:12 +0530

    SOC-5780: mdio_bus: Add suspend_late and early_resume callbacks
    suspend_late and early_resume callbacks are required for mdio bus
    when PHY(s) on mdio bus need to be suspended 'after' corresponding
    controller has been suspended and need to be powered on 'before'
    the controller resume sequence starts. This is the case with Broadcom
    USB 3.0 PHY registered under mdio bus for Northstar2 platform.
    
    Change-Id: I0a9d1158995d8d0d45a40bf487fa7c29ce373875
    Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/46103
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: vpx_smoketest status <vpx_smoketest@broadcom.com>
    Tested-by: CCXSW <ccxswbuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/net/phy/mdio_bus.c
+++ drivers/net/phy/mdio_bus.c
@@ -701,12 +701,34 @@ static int mdio_bus_restore(struct device *dev)
 	return 0;
 }
 
+static int mdio_bus_suspend_late(struct device *dev)
+{
+	struct mdio_device *mdio = to_mdio_device(dev);
+
+	if (mdio->pm_ops && mdio->pm_ops->suspend_late)
+		return mdio->pm_ops->suspend_late(dev);
+
+	return 0;
+}
+
+static int mdio_bus_resume_early(struct device *dev)
+{
+	struct mdio_device *mdio = to_mdio_device(dev);
+
+	if (mdio->pm_ops && mdio->pm_ops->resume_early)
+		return mdio->pm_ops->resume_early(dev);
+
+	return 0;
+}
+
 static const struct dev_pm_ops mdio_bus_pm_ops = {
 	.suspend = mdio_bus_suspend,
 	.resume = mdio_bus_resume,
 	.freeze = mdio_bus_suspend,
 	.thaw = mdio_bus_resume,
 	.restore = mdio_bus_restore,
+	.suspend_late = mdio_bus_suspend_late,
+	.resume_early = mdio_bus_resume_early,
 };
 
 #define MDIO_BUS_PM_OPS (&mdio_bus_pm_ops)
