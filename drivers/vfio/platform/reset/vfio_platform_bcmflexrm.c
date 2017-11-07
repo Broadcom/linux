0f486af7562c3ba2a08f4138325bcb65f6edcd59
Anup Patel <anup.patel@broadcom.com>
2017-11-07 16:47:01 +0530

    SOC-5975: vfio: platform: bcmflexrm: Syncup with upcoming Linux-4.15
    vfio: platform: bcmflexrm: Syncup with upcoming Linux-4.15
    
    This patch syncup VFIO FlexRM reset driver with upcoming Linux-4.15
    
    DO NOT UPSTREAM !!!
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Change-Id: Ieaf1cd6aea49d010eaae92c14c3a0417c8e1e1f6
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/50817
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: CCXSW <ccxsw.build@broadcom.com>
    Tested-by: CCXSW <ccxsw.build@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/vfio/platform/reset/vfio_platform_bcmflexrm.c
+++ drivers/vfio/platform/reset/vfio_platform_bcmflexrm.c
@@ -1,17 +1,14 @@
 /*
  * Copyright (C) 2017 Broadcom
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
