476807f3ae21fc0eed5dc80f423a054d06ebcc23
Abhishek Shah <abhishek.shah@broadcom.com>
2018-01-04 12:54:08 +0530

    SOC-6039: PCI: iproc: handle failures during probe
    Current code does not take care of releasing taken resources
    in case of failure(s).
    
    For example, timer setup is done before inbound/outbound mapping
    configuration and if one of the mapping fails, timer is still running,
    but the pcie resources including timer callback function are freed,
    resulting in crash when timer expires for the first time.
    
    This issue is seen during kexec reboot. It is hidden in primary
    kernel because mapping does not fail there, but in secondary
    kernel inbound mapping fails.
    
    Change-Id: I0198991d12632528a1778aebb8b9d593f97273b1
    Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/53842
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: CCXSW <ccxsw.build@broadcom.com>
    Reviewed-by: CCXSW <ccxsw.build@broadcom.com>
    Reviewed-by: Vikram Prakash <vikram.prakash@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/57397

--- drivers/pci/host/pcie-iproc.c
+++ drivers/pci/host/pcie-iproc.c
@@ -1786,6 +1786,14 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 	if (ret)
 		return ret;
 
+#if CONFIG_PM_SLEEP
+	pcie->paxb_map_regs = devm_kcalloc(dev, IPROC_PCIE_MAX_NUM_REG * 2,
+					   sizeof(*pcie->paxb_map_regs),
+					   GFP_KERNEL);
+	if (!pcie->paxb_map_regs)
+		return -ENOMEM;
+#endif
+
 	ret = phy_init(pcie->phy);
 	if (ret) {
 		dev_err(dev, "unable to initialize PCIe PHY\n");
@@ -1800,6 +1808,18 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 
 	iproc_pcie_reset(pcie);
 
+	if (pcie->need_ob_cfg) {
+		ret = iproc_pcie_map_ranges(pcie, res);
+		if (ret) {
+			dev_err(dev, "map failed\n");
+			goto err_power_off_phy;
+		}
+	}
+
+	ret = iproc_pcie_map_dma_ranges(pcie);
+	if (ret && ret != -ENOENT)
+		goto err_power_off_phy;
+
 	if (pcie->idm) {
 		val = readl(pcie->idm + IDM_ERROR_LOG_CONTROL_OFFSET);
 		val &= ~TIMEOUT_INTERRUPT & ~TIMEOUT_RESET &
@@ -1813,18 +1833,6 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 			  msecs_to_jiffies(pcie->link_poll_interval));
 	}
 
-	if (pcie->need_ob_cfg) {
-		ret = iproc_pcie_map_ranges(pcie, res);
-		if (ret) {
-			dev_err(dev, "map failed\n");
-			goto err_power_off_phy;
-		}
-	}
-
-	ret = iproc_pcie_map_dma_ranges(pcie);
-	if (ret && ret != -ENOENT)
-		goto err_power_off_phy;
-
 #ifdef CONFIG_ARM
 	pcie->sysdata.private_data = pcie;
 	sysdata = &pcie->sysdata;
@@ -1832,13 +1840,6 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 	sysdata = pcie;
 #endif
 
-#if CONFIG_PM_SLEEP
-	pcie->paxb_map_regs = devm_kcalloc(dev, IPROC_PCIE_MAX_NUM_REG * 2,
-					   sizeof(*pcie->paxb_map_regs),
-					   GFP_KERNEL);
-	if (!pcie->paxb_map_regs)
-		return -ENOMEM;
-#endif
 
 	is_link_active = iproc_pcie_check_link(pcie);
 	if (is_link_active)
@@ -1847,7 +1848,7 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 	ret = iproc_pcie_intx_enable(pcie);
 	if (ret) {
 		dev_err(dev, "failed to enable INTx\n");
-		goto err_power_off_phy;
+		goto err_unregister_bad_mode;
 	}
 
 	if (IS_ENABLED(CONFIG_PCI_MSI))
@@ -1865,12 +1866,17 @@ int iproc_pcie_setup(struct iproc_pcie *pcie, struct list_head *res)
 		ret = iproc_pcie_detect_enable(pcie);
 		if (ret < 0) {
 			dev_err(dev, "failed to enable: %d\n", ret);
-			goto err_power_off_phy;
+			goto err_unregister_bad_mode;
 		}
 	}
 
 	return 0;
 
+err_unregister_bad_mode:
+	if (pcie->idm) {
+		del_timer_sync(&pcie->timer_hdlr);
+		cancel_delayed_work_sync(&pcie->work);
+	}
 err_power_off_phy:
 	phy_power_off(pcie->phy);
 err_exit_phy:
@@ -1881,7 +1887,11 @@ EXPORT_SYMBOL(iproc_pcie_setup);
 
 int iproc_pcie_remove(struct iproc_pcie *pcie)
 {
-	cancel_delayed_work_sync(&pcie->work);
+	if (pcie->idm) {
+		del_timer_sync(&pcie->timer_hdlr);
+		cancel_delayed_work_sync(&pcie->work);
+	}
+
 	pci_stop_root_bus(pcie->root_bus);
 	pci_remove_root_bus(pcie->root_bus);
 
@@ -2033,9 +2043,10 @@ EXPORT_SYMBOL(iproc_pcie_resume);
 
 int iproc_pcie_suspend(struct iproc_pcie *pcie)
 {
-	flush_delayed_work(&pcie->work);
-	if (pcie->idm)
+	if (pcie->idm) {
+		flush_delayed_work(&pcie->work);
 		del_timer_sync(&pcie->timer_hdlr);
+	}
 
 	if (pcie->msi)
 		iproc_msi_disable(pcie->msi);
