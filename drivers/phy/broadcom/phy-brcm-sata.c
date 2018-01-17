4e85f2d0b31302cf24fbc9d69a0ca44cc3b813c6
Srinath Mannam <srinath.mannam@broadcom.com>
2018-01-17 16:21:02 +0530

    SOC-6463: SR B0 Fix SATA Invert Tx polarity
    phy: brcm-sata: SR B0 Fix SATA Invert Tx polarity
    
    HW fix added for Invert Tx polarity issue in SATA phy of SR B0
    chip. So the SW fix is not required.
    
    Signed-off-by: Srinath Mannam <srinath.mannam@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/54808
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: CCXSW <ccxsw.build@broadcom.com>
    Tested-by: CCXSW <ccxsw.build@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    (cherry picked from commit 66b0a8ce0012a2c1c475b4ba35539dcb4d80bb2f)
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/56761
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    
    Change-Id: Iad6ea4627fc8727cef91b0ca6f053b0a2d1ca7a0
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/57427
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Vikram Prakash <vikram.prakash@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/phy/broadcom/phy-brcm-sata.c
+++ drivers/phy/broadcom/phy-brcm-sata.c
@@ -107,10 +107,6 @@ enum sata_phy_regs {
 	PLL1_ACTRL3				= 0x83,
 	PLL1_ACTRL4				= 0x84,
 
-	TX_REG_BANK				= 0x070,
-	TX_ACTRL0				= 0x80,
-	TX_ACTRL0_TXPOL_FLIP			= BIT(6),
-
 	AEQRX_REG_BANK_0			= 0xd0,
 	AEQ_CONTROL1				= 0x81,
 	AEQ_CONTROL1_ENABLE			= BIT(2),
@@ -460,10 +456,6 @@ static int brcm_sr_sata_init(struct brcm_sata_port *port)
 		dev_err(dev, "port%d PLL did not lock\n", port->portnum);
 		return -ETIMEDOUT;
 	}
-
-	/* Invert Tx polarity */
-	brcm_sata_phy_wr(base, TX_REG_BANK, TX_ACTRL0,
-			 ~TX_ACTRL0_TXPOL_FLIP, TX_ACTRL0_TXPOL_FLIP);
 
 	/* Configure OOB control to handle 100MHz reference clock */
 	val = ((0xc << OOB_CTRL1_BURST_MAX_SHIFT) |
