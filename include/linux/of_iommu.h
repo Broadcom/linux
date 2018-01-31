bd05223d22532ef9fe5aeeab35cdded3ff6f6658
Jitendra Bhivare <jitendra.bhivare@broadcom.com>
2018-01-31 22:25:31 +0530

    SOC-6462: arm64: Reserve IOMMU DMA regions using DT
    Stingray SoC provides limited windows to access memory sparsed as per ARM
    memory map to the devices sitting behind IOMMU.
    
    PAXBv2 PCIe RC in-bound windows covers the DDR space but not the entire
    40-bit 1TB space used by IOMMU code as per DMA mask set.
    
    Current IOMMU code does not provide a hook to reserve these holes. Hence
    use already defined of_get_dma_window to get reserved DMA regions from DT.
    
    To reserve the regions for IOVA allocation, ranges can be specified in DT
    as <busno prot bus_addr size>. These regions are reserved when device
    behind SMMU gets added and arm_smmu_get_resv_regions gets called.
    
    This code is useful in reserving region for GICv3 ITS MSI too in SMMU.
    
    Change-Id: Ia8dd727ca5b85cd4fa8c96c7deef9c7b3501fe43
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/55987
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- include/linux/of_iommu.h
+++ include/linux/of_iommu.h
@@ -6,20 +6,27 @@
 #include <linux/iommu.h>
 #include <linux/of.h>
 
+struct of_iommu_dma_window {
+	u32 busno;
+	u32 prot;
+	u64 bus_addr;
+	u64 size;
+};
+
 #ifdef CONFIG_OF_IOMMU
 
-extern int of_get_dma_window(struct device_node *dn, const char *prefix,
-			     int index, unsigned long *busno, dma_addr_t *addr,
-			     size_t *size);
+int of_get_dma_window(struct device_node *dn, const char *prefix, int *index,
+		      struct of_iommu_dma_window *dma_window);
 
 extern const struct iommu_ops *of_iommu_configure(struct device *dev,
 					struct device_node *master_np);
 
+void of_iommu_resv_dma_regions(struct device_node *np, struct list_head *list);
+
 #else
 
 static inline int of_get_dma_window(struct device_node *dn, const char *prefix,
-			    int index, unsigned long *busno, dma_addr_t *addr,
-			    size_t *size)
+			int *index, struct of_iommu_dma_window *dma_window)
 {
 	return -EINVAL;
 }
@@ -28,6 +35,12 @@ static inline const struct iommu_ops *of_iommu_configure(struct device *dev,
 					 struct device_node *master_np)
 {
 	return NULL;
+}
+
+static inline int of_iommu_resv_dma_regions(struct device_node *np,
+					    struct list_head *list)
+{
+	return -EINVAL;
 }
 
 #endif	/* CONFIG_OF_IOMMU */
