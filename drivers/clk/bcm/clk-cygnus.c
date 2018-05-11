cb102bd75419b9f8f880757bf12bb4560fa567ca
Lori Hikichi <lori.hikichi@broadcom.com>
2018-05-10 17:56:51 -0700

    VOIP-7253: clk: iproc: Change audiomux compatibility string
    The compatibility string for the audio mux does not need to be
    specific to each port.  Use one generic string with varying
    properties.
    
    Change-Id: I6339a4d7b0de459d52a7a97a19eb5ef389736e50
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/62559
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Reviewed-by: Henrik Christensen <henrik.christensen@broadcom.com>

--- drivers/clk/bcm/clk-cygnus.c
+++ drivers/clk/bcm/clk-cygnus.c
@@ -324,7 +324,4 @@ static void __init audiomux_init(struct device_node *node)
 {
 	iproc_audiomux_setup(node);
 }
-CLK_OF_DECLARE(cygnus_audiomux_clk0, "brcm,cygnus-ssp0-clk", audiomux_init);
-CLK_OF_DECLARE(cygnus_audiomux_clk1, "brcm,cygnus-ssp1-clk", audiomux_init);
-CLK_OF_DECLARE(cygnus_audiomux_clk2, "brcm,cygnus-ssp2-clk", audiomux_init);
-CLK_OF_DECLARE(cygnus_audiomux_clk3, "brcm,cygnus-spdiftx-clk", audiomux_init);
+CLK_OF_DECLARE(cygnus_audiomux_clk0, "brcm,iproc-mux-clk", audiomux_init);
