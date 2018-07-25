4620ed6dcf72ff43ae21fba75b47cdb321c8390e
Srinath Mannam <srinath.mannam@broadcom.com>
2018-07-25 20:49:51 +0530

    SOC-7328: thermal: stingray: TMONs initialize in one instance
    All available TMONs are initialized together in one
    instance of driver probe call.
    Available TMONs information taken from tmon-mask parameter.
    
    Change-Id: I9253b5199b68c4accce8e1ac2cc15286c7b4d6ef
    Signed-off-by: Srinath Mannam <srinath.mannam@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/66821
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/thermal/broadcom/sr-thermal.c
+++ drivers/thermal/broadcom/sr-thermal.c
@@ -17,20 +17,45 @@
 #include <linux/platform_device.h>
 #include <linux/thermal.h>
 
-#define TMON_CRIT_TEMP 105000 /* temp in millidegree C */
+#define TMON_CRIT_TEMP          105000 /* temp in millidegree C */
+#define SR_TMON_MAX_LIST        6
+
+/*
+ * In stingray thermal IO memory,
+ * Total Number of available TMONs MASK is at offset 0
+ * temperature registers BASE is at 4 byte offset.
+ * Each TMON temperature register size is 4.
+ */
+#define SR_TMON_TEMP_BASE(id)   ((id) * 0x4)
+
+static const char * const sr_tmon_names[SR_TMON_MAX_LIST] = {
+	"sr_tmon_ihost0",
+	"sr_tmon_ihost1",
+	"sr_tmon_ihost2",
+	"sr_tmon_ihost3",
+	"sr_tmon_crmu",
+	"sr_tmon_nitro",
+};
+
+struct sr_tmon {
+	struct thermal_zone_device *tz;
+	unsigned int crit_temp;
+	unsigned int tmon_id;
+	struct sr_thermal *priv;
+};
 
 struct sr_thermal {
-	struct thermal_zone_device *tz;
 	struct device *dev;
 	void __iomem *regs;
-	unsigned int crit_temp;
+	struct sr_tmon tmon[SR_TMON_MAX_LIST];
 };
 
 static int sr_get_temp(struct thermal_zone_device *tz, int *temp)
 {
-	struct sr_thermal *sr_thermal = tz->devdata;
+	struct sr_tmon *tmon = tz->devdata;
+	struct sr_thermal *sr_thermal = tmon->priv;
 
-	*temp = readl(sr_thermal->regs);
+	*temp = readl(sr_thermal->regs + SR_TMON_TEMP_BASE(tmon->tmon_id));
 
 	return 0;
 }
@@ -38,7 +63,8 @@ static int sr_get_temp(struct thermal_zone_device *tz, int *temp)
 static int sr_get_trip_type(struct thermal_zone_device *tz, int trip,
 					enum thermal_trip_type *type)
 {
-	struct sr_thermal *sr_thermal = tz->devdata;
+	struct sr_tmon *tmon = tz->devdata;
+	struct sr_thermal *sr_thermal = tmon->priv;
 
 	switch (trip) {
 	case 0:
@@ -54,11 +80,12 @@ static int sr_get_trip_type(struct thermal_zone_device *tz, int trip,
 
 static int sr_get_trip_temp(struct thermal_zone_device *tz, int trip, int *temp)
 {
-	struct sr_thermal *sr_thermal = tz->devdata;
+	struct sr_tmon *tmon = tz->devdata;
+	struct sr_thermal *sr_thermal = tmon->priv;
 
 	switch (trip) {
 	case 0:
-		*temp = sr_thermal->crit_temp;
+		*temp = tmon->crit_temp;
 		break;
 	default:
 		dev_err(sr_thermal->dev,
@@ -70,7 +97,7 @@ static int sr_get_trip_temp(struct thermal_zone_device *tz, int trip, int *temp)
 
 static int sr_set_trip_temp(struct thermal_zone_device *tz, int trip, int temp)
 {
-	struct sr_thermal *sr_thermal = tz->devdata;
+	struct sr_tmon *tmon = tz->devdata;
 
 	switch (trip) {
 	case 0:
@@ -80,7 +107,7 @@ static int sr_set_trip_temp(struct thermal_zone_device *tz, int trip, int temp)
 		 * purpose, even if it's more than the recommended
 		 * critical temperature.
 		 */
-		sr_thermal->crit_temp = temp;
+		tmon->crit_temp = temp;
 		break;
 	default:
 		return -EINVAL;
@@ -99,11 +126,16 @@ static int sr_thermal_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct sr_thermal *sr_thermal;
+	struct sr_tmon *tmon;
 	struct resource *res;
+	uint32_t sr_tmon_list = 0;
+	unsigned int i;
+	int ret;
 
 	sr_thermal = devm_kzalloc(dev, sizeof(*sr_thermal), GFP_KERNEL);
 	if (!sr_thermal)
 		return -ENOMEM;
+
 	sr_thermal->dev = dev;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
@@ -114,27 +146,52 @@ static int sr_thermal_probe(struct platform_device *pdev)
 		return PTR_ERR(sr_thermal->regs);
 	}
 
-	/* initialize tmon value to 0 */
-	writel(0, sr_thermal->regs);
-	sr_thermal->crit_temp = TMON_CRIT_TEMP;
+	ret = device_property_read_u32(dev, "brcm,tmon-mask", &sr_tmon_list);
+	if (ret)
+		return ret;
 
-	sr_thermal->tz = thermal_zone_device_register(dev_name(dev), 1, 1,
-							 sr_thermal,
-							 &sr_thermal_ops,
-							 NULL, 1000, 1000);
-	if (IS_ERR(sr_thermal->tz))
-		return PTR_ERR(sr_thermal->tz);
+	for (i = 0; i < SR_TMON_MAX_LIST; i++) {
 
+		if (!(sr_tmon_list & BIT(i)))
+			continue;
+
+		/* Flush temperature registers */
+		writel(0, sr_thermal->regs + SR_TMON_TEMP_BASE(i));
+		tmon = &sr_thermal->tmon[i];
+		tmon->crit_temp = TMON_CRIT_TEMP;
+		tmon->tmon_id = i;
+		tmon->priv = sr_thermal;
+		tmon->tz = thermal_zone_device_register(sr_tmon_names[i],
+				1, 1,
+				tmon,
+				&sr_thermal_ops,
+				NULL, 1000, 1000);
+		if (IS_ERR(tmon->tz))
+			goto err_exit;
+
+		dev_info(dev, "%s: registered\n", sr_tmon_names[i]);
+	}
 	platform_set_drvdata(pdev, sr_thermal);
 
 	return 0;
+
+err_exit:
+	while (--i >= 0) {
+		if (sr_thermal->tmon[i].tz)
+			thermal_zone_device_unregister(sr_thermal->tmon[i].tz);
+	}
+
+	return PTR_ERR(tmon->tz);
 }
 
 static int sr_thermal_remove(struct platform_device *pdev)
 {
 	struct sr_thermal *sr_thermal = platform_get_drvdata(pdev);
+	unsigned int i;
 
-	thermal_zone_device_unregister(sr_thermal->tz);
+	for (i = 0; i < SR_TMON_MAX_LIST; i++)
+		if (sr_thermal->tmon[i].tz)
+			thermal_zone_device_unregister(sr_thermal->tmon[i].tz);
 
 	return 0;
 }
