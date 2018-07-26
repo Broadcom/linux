Broadcom/linux

commit bb0cbd7b9a5211d29ab1b3e87612a7ca1b3f5f2e

SOC-7271: Add nvme-lpm-ssr support
1. Define power-loss ISR implementation
2. Export nvme-lpm interface to user space
3. SSR read/write/arm/disarm support
4. Use mailbox mechanism to send SSR request to CRMU
5. Reserve memory for NVMe data required for NVMe-CRMU communication

Change-Id: I128cf535311ef0ff35ebaf598368572cdc6466da
Signed-off-by: Pramod Kumar <pramod.kumar@broadcom.com>
Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>
Reviewed-on: http://gerrit-ccxsw.broadcom.net/63643
Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
Reviewed-by: Vikram Prakash <vikram.prakash@broadcom.com>
Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
Reviewed-by: Scott Branden <scott.branden@broadcom.com>

drivers/misc/nvme-lpm-ssr.c
@@ -1,0 +1,395 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright(c) 2018 Broadcom
+ */
+#include <asm/barrier.h>
+#include <asm/spinlock.h>
+#include <linux/bcm_iproc_mailbox.h>
+#include <linux/cdev.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <linux/gpio/consumer.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/mailbox_client.h>
+#include <linux/module.h>
+#include <linux/nvme-lpm-ssr.h>
+#include <linux/of_address.h>
+#include <linux/platform_device.h>
+#include <linux/smp.h>
+#include <linux/uaccess.h>
+#include <uapi/asm-generic/errno.h>
+
+/*
+ * Pass SSR command to CRMU
+ * Populate SSR Structure in shared memory which would be flashed to
+ * NVM memory via CRMU
+ * Response - none
+ */
+#define M0_IPC_M0_CMD_SSR_MSG 0x82
+
+struct nvme_lpm {
+	struct device *dev;
+	struct mbox_client client;
+	struct mbox_chan *mbox_chan;
+	void *shared_mem;
+	spinlock_t shmlock;
+	void *shared_nvme_data;
+	struct gpio_desc *gpiod;
+	int irq;
+	struct class *class;
+	struct cdev cdev;
+	struct device *class_dev;
+	dev_t devt;
+	unsigned int ssr_state_armed;
+};
+
+static int iproc_mbox_send_msg(struct nvme_lpm *nvme_lpm,
+				struct ssr_wrapper *lwrap)
+{
+	int ret, i;
+	struct iproc_mbox_msg msg;
+	struct ssr_wrapper *wrap = nvme_lpm->shared_mem;
+
+	spin_lock(&nvme_lpm->shmlock);
+	memcpy(wrap, lwrap, sizeof(*lwrap));
+	msg.cmd = M0_IPC_M0_CMD_SSR_MSG;
+	msg.param = 0;
+	msg.wait_ack = false;
+
+	wrap->crmu_maia_valid_response = 0x0;
+	wrap->maia_crmu_valid_request = MAIA_CRMU_VALID_REQUEST;
+	dsb(sy);
+	ret = mbox_send_message(nvme_lpm->mbox_chan, &msg);
+	if (ret < 0) {
+		dev_err(nvme_lpm->dev, "Failed to send mbox msg ret:%d\n", ret);
+		goto fail;
+	}
+	ret = 0;
+
+	mbox_client_txdone(nvme_lpm->mbox_chan, 0);
+
+	/* Wait for completion */
+	for (i = 0; i < MAX_CRMU_RESPONSE_TIMEOUT; i++)
+		if (wrap->ssr_cmd_id != 0)
+			udelay(100);
+		else
+			break;
+
+	if (i == MAX_CRMU_RESPONSE_TIMEOUT) {
+		dev_err(nvme_lpm->dev, "CRMU response timed out\n");
+		ret = -ETIMEDOUT;
+	}
+
+	if (wrap->crmu_maia_valid_response != CRMU_MAIA_VALID_RESPONSE)
+		ret = -ENODATA;
+fail:
+	spin_unlock(&nvme_lpm->shmlock);
+	return ret;
+}
+
+static int nvme_lpm_get_ssr(struct nvme_lpm *nvme_lpm, void __user *argp)
+{
+	int ret = 0;
+	struct ssr_wrapper wrap = {0,};
+
+	/* send msg to CRMU */
+	wrap.ssr_cmd_id = NVME_LPM_CMD_GET_SSR;
+	ret = iproc_mbox_send_msg(nvme_lpm, &wrap);
+	if (ret)
+		goto fail;
+
+	/* copy SSR */
+	if (copy_to_user(argp, &wrap.ssr, SSR_SIZE)) {
+		dev_err(nvme_lpm->dev, "Failed to copy to user space\n");
+		ret = -EFAULT;
+	}
+fail:
+	return ret;
+}
+
+static int nvme_lpm_erase_ssr(struct nvme_lpm *nvme_lpm, void __user *argp)
+{
+	struct ssr_wrapper wrap = {0,};
+
+	wrap.ssr_cmd_id = NVME_LPM_CMD_ERASE_SSR;
+	wrap.ssr.state = SSR_STATE_INIT;
+
+	/* send msg to CRMU */
+	return iproc_mbox_send_msg(nvme_lpm, &wrap);
+}
+
+static int nvme_lpm_arm_ssr(struct nvme_lpm *nvme_lpm, void __user *argp)
+{
+	struct ssr_wrapper wrap = {0,};
+	struct armed_ssr armed_ssr;
+	int ret;
+
+	if (copy_from_user(&armed_ssr, argp, sizeof(struct armed_ssr))) {
+		dev_err(nvme_lpm->dev, "Failed to copy armed ssr from user\n");
+		return -EFAULT;
+	}
+
+	wrap.ssr_cmd_id = NVME_LPM_CMD_ARM_SSR;
+	wrap.ssr.state = SSR_STATE_ARM;
+	wrap.ssr.sequence = armed_ssr.sequence;
+
+	/* send msg to CRMU */
+	ret = iproc_mbox_send_msg(nvme_lpm, &wrap);
+	if (ret)
+		nvme_lpm->ssr_state_armed = false;
+	else
+		nvme_lpm->ssr_state_armed = true;
+
+	return ret;
+}
+
+static int nvme_lpm_disarm_cmd(struct nvme_lpm *nvme_lpm, void __user *argp)
+{
+	struct ssr_wrapper wrap = {0,};
+	struct disarmed_ssr disarmed_ssr;
+	int ret;
+
+	ret = copy_from_user(&disarmed_ssr, argp, sizeof(struct disarmed_ssr));
+	if (ret) {
+		dev_err(nvme_lpm->dev, "Failed to copy disarmed info\n");
+		goto out;
+	}
+	wrap.ssr_cmd_id = NVME_LPM_CMD_DISARM_SSR;
+	wrap.ssr.state = SSR_STATE_DISARM;
+	wrap.ssr.sequence = disarmed_ssr.sequence;
+
+	/* send msg to CRMU */
+	ret = iproc_mbox_send_msg(nvme_lpm, &wrap);
+out:
+	return ret;
+}
+
+static int nvme_lpm_trigger_ssr(struct nvme_lpm *nvme_lpm)
+{
+	/* TODO: Trigger NVMe backup transactions */
+
+	return 0;
+}
+
+/* IOCTL interface through character device */
+static int nvme_dev_open(struct inode *inode, struct file *file)
+{
+	file->private_data = container_of(inode->i_cdev,
+					  struct nvme_lpm, cdev);
+
+	return 0;
+}
+
+static long nvme_dev_ioctl(struct file *file, unsigned int cmd,
+			   unsigned long arg)
+{
+	struct nvme_lpm *nvme_lpm = file->private_data;
+	void __user *argp = (void __user *)arg;
+	int ret = -1;
+
+	switch (cmd) {
+	case NVME_LPM_IOCTL_GET_SSR:
+		ret = nvme_lpm_get_ssr(nvme_lpm, argp);
+		break;
+	case NVME_LPM_IOCTL_ERASE_SSR:
+		ret = nvme_lpm_erase_ssr(nvme_lpm, argp);
+		break;
+	case NVME_LPM_IOCTL_ARM_SSR:
+		ret = nvme_lpm_arm_ssr(nvme_lpm, argp);
+		break;
+	case NVME_LPM_IOCTL_DISARM_SSR:
+		ret = nvme_lpm_disarm_cmd(nvme_lpm, argp);
+		break;
+	case NVME_LPM_IOCTL_TRIGGER_SSR:
+		ret = nvme_lpm_trigger_ssr(nvme_lpm);
+		break;
+	default:
+		return -ENOENT;
+	}
+	return ret;
+}
+
+
+static irqreturn_t iproc_gpio_isr(int irq, void *drv_ctx)
+{
+	struct nvme_lpm *nvme_lpm = (struct nvme_lpm *)drv_ctx;
+
+	/* Actions to be taken when GPIO interrupt is generated */
+	console_silent();
+	if (nvme_lpm->ssr_state_armed == true) {
+		/* TBD: need to take ssr check-point */
+		/* TBD: Trigger NVMe backup transaction */
+		/* TBD: need to take ssr check-point */
+	}
+	smp_send_stop();
+	set_cpu_online(smp_processor_id(), false);
+	cpu_die();
+
+	return IRQ_HANDLED;
+}
+
+static const struct file_operations nvme_dev_fops = {
+	.owner		= THIS_MODULE,
+	.open		= nvme_dev_open,
+	.unlocked_ioctl	= nvme_dev_ioctl,
+	.compat_ioctl	= nvme_dev_ioctl,
+};
+
+static int nvme_lpm_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct nvme_lpm *nvme_lpm;
+	struct device *dev = &pdev->dev;
+	struct resource *res, reg;
+	struct device_node *np;
+
+	nvme_lpm = devm_kzalloc(dev, sizeof(struct nvme_lpm), GFP_KERNEL);
+	if (!nvme_lpm) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	nvme_lpm->dev = dev;
+	spin_lock_init(&nvme_lpm->shmlock);
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	nvme_lpm->shared_mem = devm_memremap(dev, res->start,
+					resource_size(res), MEMREMAP_WB);
+	if (IS_ERR(nvme_lpm->shared_mem)) {
+		dev_err(dev, "failed to get io address\n");
+		ret = PTR_ERR(nvme_lpm->shared_mem);
+		goto out;
+	}
+
+	np = of_parse_phandle(dev->of_node, "memory-region", 0);
+	if (!np) {
+		dev_err(dev, "No memory-region specified\n");
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	ret = of_address_to_resource(np, 0, &reg);
+	if (ret) {
+		dev_err(dev, "No memory address assigned to the region\n");
+		ret = -ENOMEM;
+		goto out_put_node;
+	}
+
+	nvme_lpm->shared_nvme_data = devm_memremap(dev, reg.start,
+					resource_size(&reg), MEMREMAP_WB);
+	if (IS_ERR(nvme_lpm->shared_nvme_data)) {
+		dev_err(dev, "Failed to remap pci_lpm_mem region\n");
+		ret = PTR_ERR(nvme_lpm->shared_nvme_data);
+		goto out_put_node;
+	}
+
+	nvme_lpm->gpiod = devm_gpiod_get(dev, "intrpt", GPIOD_IN);
+	if (IS_ERR(nvme_lpm->gpiod)) {
+		dev_err(dev, "Failed to get interrupt gpio\n");
+		ret = PTR_ERR(nvme_lpm->gpiod);
+		goto out_put_node;
+	}
+
+	nvme_lpm->irq = gpiod_to_irq(nvme_lpm->gpiod);
+	if (nvme_lpm->irq < 0) {
+		dev_err(dev, "Failed to get interrupt gpio IRQ\n");
+		ret = nvme_lpm->irq;
+		goto out_put_node;
+	}
+
+	/* init irq */
+	ret = devm_request_irq(dev, nvme_lpm->irq, iproc_gpio_isr,
+			       IRQF_TRIGGER_RISING,
+			       dev_name(dev), nvme_lpm);
+	if (ret) {
+		dev_err(dev, "Failed to register interrupt\n");
+		goto out_put_node;
+	}
+
+	/* Request mailbox channel. */
+	nvme_lpm->client.dev          = &pdev->dev;
+	nvme_lpm->client.tx_block     = false;
+	nvme_lpm->client.tx_tout      = 1;
+	nvme_lpm->client.knows_txdone = true;
+	nvme_lpm->mbox_chan = mbox_request_channel(&nvme_lpm->client, 0);
+	if (IS_ERR(nvme_lpm->mbox_chan)) {
+		dev_err(dev, "unable to get mbox channel\n");
+		ret = PTR_ERR(nvme_lpm->mbox_chan);
+		goto out_put_node;
+	}
+	dev_info(dev, "Mailbox registration done\n");
+	ret = alloc_chrdev_region(&nvme_lpm->devt, 0, 1, "nvme-lpm");
+	if (ret) {
+		dev_err(dev, "cannot get add chrdev, err:%d\n", ret);
+		goto out_mbox;
+	}
+	cdev_init(&nvme_lpm->cdev, &nvme_dev_fops);
+	ret = cdev_add(&nvme_lpm->cdev, nvme_lpm->devt, 1);
+	if (ret) {
+		dev_err(dev, "cannot get add chrdev, err:%d\n", ret);
+		goto out_region;
+	}
+	dev_info(dev, "Character device created\n");
+
+	nvme_lpm->class = class_create(THIS_MODULE, "nvme-lpm");
+	if (IS_ERR(nvme_lpm->class)) {
+		ret = PTR_ERR(nvme_lpm->class);
+		goto out_cdev;
+	}
+	dev_info(dev, "Class creation done\n");
+
+	nvme_lpm->class_dev = device_create(nvme_lpm->class, nvme_lpm->dev,
+				  nvme_lpm->devt, nvme_lpm, "nvme-lpm");
+	if (IS_ERR(nvme_lpm->class_dev)) {
+		dev_err(dev, "failed to create device\n");
+		ret = PTR_ERR(nvme_lpm->class_dev);
+		goto out_class;
+	}
+	dev_info(dev, "device got added\n");
+	return ret;
+out_class:
+	class_destroy(nvme_lpm->class);
+out_cdev:
+	cdev_del(&nvme_lpm->cdev);
+out_region:
+	unregister_chrdev_region(nvme_lpm->devt, 1);
+out_mbox:
+	mbox_free_channel(nvme_lpm->mbox_chan);
+out_put_node:
+	of_node_put(np);
+out:
+	return ret;
+}
+
+static int nvme_lpm_remove(struct platform_device *pdev)
+{
+	struct nvme_lpm *nvme_lpm = platform_get_drvdata(pdev);
+
+	class_destroy(nvme_lpm->class);
+	cdev_del(&nvme_lpm->cdev);
+	unregister_chrdev_region(nvme_lpm->devt, 1);
+	mbox_free_channel(nvme_lpm->mbox_chan);
+
+	return 0;
+}
+
+static const struct of_device_id nvme_lpm_of_match[] = {
+	{ .compatible = "brcm,nvme-lpm-data-backup", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, nvme_lpm_of_match);
+
+struct platform_driver nvme_lpm_driver = {
+	.driver = {
+		.name = "nvme-lpm-data-backup",
+		.of_match_table = nvme_lpm_of_match,
+	},
+	.probe = nvme_lpm_probe,
+	.remove = nvme_lpm_remove,
+};
+module_platform_driver(nvme_lpm_driver);
+MODULE_AUTHOR("Broadcom");
+MODULE_DESCRIPTION("Broadcom nvme lpm driver");
+MODULE_LICENSE("GPL v2");

drivers/misc/Kconfig
@@ -529,4 +529,15 @@ config BCM_IPROC_DTE
 
           If unsure, say N.
 
+config NVME_LPM_SSR
+        bool "NVME LPM data-back support"
+        depends on ARCH_BCM_IPROC || COMPILE_TEST
+        depends on OF_GPIO
+        help
+          This module enables support for udpating SSR struct stored in NVM
+          memory and initiate data-back to nvme once poweloss interrupt gets
+          triggered indicating for data-backup.
+
+          If unsure, say N.
+
 endmenu

drivers/misc/Makefile
@@ -69,6 +69,7 @@ lkdtm-$(CONFIG_LKDTM)		+= lkdtm_perms.o
 lkdtm-$(CONFIG_LKDTM)		+= lkdtm_refcount.o
 lkdtm-$(CONFIG_LKDTM)		+= lkdtm_rodata_objcopy.o
 lkdtm-$(CONFIG_LKDTM)		+= lkdtm_usercopy.o
+obj-$(CONFIG_NVME_LPM_SSR)	+= nvme-lpm-ssr.o
 
 KCOV_INSTRUMENT_lkdtm_rodata.o	:= n
 

include/linux/nvme-lpm-ssr.h
@@ -1,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright(c) 2018 Broadcom
+ */
+
+#ifndef __NVME_LPM_SSR_H__
+#define __NVME_LPM_SSR_H__
+
+#include <stddef.h>
+
+enum ssr_states {
+	SSR_STATE_INVALID,
+	SSR_STATE_INIT,
+	SSR_STATE_DISARM,
+	SSR_STATE_ARM,
+	SSR_STATE_POWERLOSS,
+	SSR_STATE_INPROGRESS,
+	SSR_STATE_COMPLETE,
+	SSR_STATE_ERROR,
+};
+
+struct armed_ssr {
+	uint32_t sequence;
+	uint64_t memory_address;
+	uint64_t disk_address;
+	uint64_t length;
+} __attribute__((packed));
+
+struct disarmed_ssr {
+	uint32_t sequence;
+} __attribute__((packed));
+
+struct state_save_checkpoint {
+	uint32_t voltage;
+	uint64_t timestamp;
+} __attribute__((packed));
+
+struct ssr {
+	uint8_t state;
+	uint32_t sequence;
+	uint32_t nvme_error_code;
+	uint32_t ddr_error_code;
+	uint32_t l3_cache_err;
+	struct state_save_checkpoint power_loss;
+	struct state_save_checkpoint in_progress;
+	struct state_save_checkpoint complete;
+} __attribute__((packed));
+#define SSR_SIZE sizeof(struct ssr)
+
+enum {
+	NVME_LPM_CMD_GET_SSR = 0x53535201, /* cmd format in ASCII SSR<x>*/
+	NVME_LPM_CMD_ERASE_SSR,
+	NVME_LPM_CMD_ARM_SSR,
+	NVME_LPM_CMD_DISARM_SSR,
+	NVME_LPM_CMD_TRIGGER_SSR,
+};
+
+#define NVM_SSR_OFFSET	0x0
+#define OS_CRMU_SSRCMD_MEM 0x8f100100
+
+struct ssr_wrapper {
+	/*
+	 * MAIA must populate this signature indicating validity of cmd
+	 */
+#define MAIA_CRMU_VALID_REQUEST		0xbabebabe
+	uint32_t maia_crmu_valid_request;
+	/*
+	 * CRMU must populate this signature indicating status of command.
+	 * if done succussfully populate with CRMU_MAIA_VALID_RESPONSE or
+	 * 0x0 as erroneous condition.
+	 */
+#define MAX_CRMU_RESPONSE_TIMEOUT 1000 /* 100 msec */
+#define CRMU_MAIA_VALID_RESPONSE	0xfaceface
+	uint32_t crmu_maia_valid_response;
+	/*
+	 * MAIA uses this to send the command to MCU. MCU should clear this
+	 * indicating that command has been processed.
+	 */
+	uint32_t ssr_cmd_id;
+
+	/*
+	 * SSR struture. It should be populated with relevant field in case
+	 * other side expects anything i.e  sequence number
+	 */
+	struct ssr ssr;
+} __attribute__((packed));
+
+#define NVME_LPM_IOCTL_MAGIC		'N'
+#define NVME_LPM_IOCTL_GET_SSR		_IOR(NVME_LPM_IOCTL_MAGIC, 1, \
+						struct ssr)
+#define NVME_LPM_IOCTL_ERASE_SSR	_IO(NVME_LPM_IOCTL_MAGIC, 2)
+#define NVME_LPM_IOCTL_ARM_SSR		_IOW(NVME_LPM_IOCTL_MAGIC, 3, \
+						struct armed_ssr)
+#define NVME_LPM_IOCTL_DISARM_SSR	_IOW(NVME_LPM_IOCTL_MAGIC, 4, \
+						struct disarmed_ssr)
+#define NVME_LPM_IOCTL_TRIGGER_SSR	_IO(NVME_LPM_IOCTL_MAGIC, 5)
+#define NVME_LPM_IOCTL_READ		_IOW(NVME_LPM_IOCTL_MAGIC, 6, \
+						struct armed_ssr)
+#define NVME_LPM_IOCTL_AP_POLL		_IO(NVME_LPM_IOCTL_MAGIC, 7)
+
+#endif

commit 7000f1b6772a36efea364df9bd3b52f2b731a685

SOC-7777: nvme-lpm-ssr: Handle SSR read IOCTL case
CRMU response data needs to be written back from shared memory
to local SSR in GET_SSR IOCTL case.

Change-Id: I3736f587aa0029b779dc43568dc0fc74c77dc785
Fixes: bb0cbd7b9a52 ("Add nvme-lpm-ssr support")
Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>
Reviewed-on: http://gerrit-ccxsw.broadcom.net/66776
Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
Reviewed-by: Vikram Prakash <vikram.prakash@broadcom.com>
Reviewed-by: Pramod Kumar <pramod.kumar@broadcom.com>
Reviewed-by: Ray Jui <ray.jui@broadcom.com>
Tested-by: Ray Jui <ray.jui@broadcom.com>

drivers/misc/nvme-lpm-ssr.c
@@ -83,10 +83,21 @@ static int iproc_mbox_send_msg(struct nvme_lpm *nvme_lpm,
 	if (i == MAX_CRMU_RESPONSE_TIMEOUT) {
 		dev_err(nvme_lpm->dev, "CRMU response timed out\n");
 		ret = -ETIMEDOUT;
+		goto fail;
 	}
 
-	if (wrap->crmu_maia_valid_response != CRMU_MAIA_VALID_RESPONSE)
+	if (wrap->crmu_maia_valid_response != CRMU_MAIA_VALID_RESPONSE) {
 		ret = -ENODATA;
+		goto fail;
+	}
+
+	/*
+	 * Data needs to be written back from shared memory to local SSR
+	 * in GET_SSR case
+	 */
+	if (lwrap->ssr_cmd_id == NVME_LPM_CMD_GET_SSR)
+		memcpy(&lwrap->ssr, &wrap->ssr, SSR_SIZE);
+
 fail:
 	spin_unlock(&nvme_lpm->shmlock);
 	return ret;