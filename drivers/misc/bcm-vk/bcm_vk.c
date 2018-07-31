3eba9ec414a7e001c19d4cd391f3b24c6f51a814
Scott Branden <scott.branden@broadcom.com>
2018-07-30 17:22:44 -0700

    SOC-7464 misc: bcm-vk: add ioctl handling
    Add ioctl handling based on current Boot Architecture doc
    and qemu device model implementation.
    To be adjusted as functionality of Valkyrie evolves.
    
    Support added for getmetadata, loadimage, accessbar (read/write), and
    reset.
    
    Change-Id: I42af4e78b5327a131de9d9dc422a16cc01d5f71e
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/67366
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/misc/bcm-vk/bcm_vk.c
+++ drivers/misc/bcm-vk/bcm_vk.c
@@ -3,6 +3,8 @@
  * Copyright(c) 2018 Broadcom
  */
 
+#include <linux/delay.h>
+#include <linux/firmware.h>
 #include <linux/fs.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
@@ -11,31 +13,293 @@
 #include <linux/pci.h>
 #include <linux/pci_ids.h>
 #include <linux/pci_regs.h>
+#include <linux/sizes.h>
+#include <linux/uaccess.h>
+#include <uapi/linux/misc/bcm_vk.h>
 
-#define DRV_MODULE_NAME "bcm-vk"
+#define DRV_MODULE_NAME		"bcm-vk"
 
-#define PCI_DEVICE_ID_VALKYRIE 0x5E87
+#define PCI_DEVICE_ID_VALKYRIE	0x5E87
 
 static DEFINE_IDA(bcm_vk_ida);
 
+#define to_bcm_vk(priv) container_of((priv), struct bcm_vk, miscdev)
+
 #define MAX_BAR 3
+enum pci_barno {
+	BAR_0 = 0,
+	BAR_1,
+	BAR_2
+};
+
+/* Location of registers of interest in BAR0 */
+#define BAR_FB_REQ		0x400
+#define BAR_CODEPUSH_ADDRESS	0x404
+#define CODEPUSH_FASTBOOT	BIT(0)
+#define CODEPUSH_FIRMWARE	BIT(1)
+#define BAR_FB_RAM_OPEN		0x40C
+#define RAM_OPEN_SRAM		BIT(16)
+#define RAM_OPEN_DDR		BIT(17)
+#define BAR_CARD_STATUS		0x410
+#define BAR_FW_STATUS		0x41C
+
+#define BAR_METADATA_VERSION	0x440
+#define BAR_FIRMWARE_VERSION	0x444
+
+/* Location of memory base addresses of interest in BAR1 */
+/* Load Boot1 to start of ITCM */
+#define BAR1_CODEPUSH_BASE_FASTBOOT	0x100000
+/* Load Boot2 to start of DDR0 */
+#define BAR1_CODEPUSH_BASE_FIRMWARE	0x300000
 
 struct bcm_vk {
 	struct pci_dev *pdev;
 	void __iomem *bar[MAX_BAR];
 	int num_irqs;
+	/* mutex to protect the ioctls */
+	struct mutex mutex;
 	struct miscdevice miscdev;
 };
+
+static inline u32 vkread32(struct bcm_vk *vk,
+			   enum pci_barno bar,
+			   uint64_t offset)
+{
+	u32 value;
+
+	value = ioread32(vk->bar[bar] + offset);
+	return value;
+}
+
+static inline void vkwrite32(struct bcm_vk *vk,
+			   u32 value,
+			   enum pci_barno bar,
+			   uint64_t offset)
+{
+	iowrite32(value, vk->bar[bar] + offset);
+}
 
 static irqreturn_t bcm_vk_irqhandler(int irq, void *dev_id)
 {
 	return IRQ_HANDLED;
 }
 
+static long bcm_vk_get_metadata(struct bcm_vk *vk, struct vk_metadata *arg)
+{
+	struct device *dev = &vk->pdev->dev;
+	struct vk_metadata metadata;
+	long ret = 0;
+
+	metadata.version = vkread32(vk, BAR_0, BAR_METADATA_VERSION);
+	dev_dbg(dev, "version=0x%x\n", metadata.version);
+	metadata.card_status = vkread32(vk, BAR_0, BAR_CARD_STATUS);
+	dev_dbg(dev, "card_status=0x%x\n", metadata.card_status);
+	metadata.firmware_version = vkread32(vk, BAR_0, BAR_FIRMWARE_VERSION);
+	dev_dbg(dev, "firmware_version=0x%x\n", metadata.firmware_version);
+	metadata.fw_status = vkread32(vk, BAR_0, BAR_FW_STATUS);
+	dev_dbg(dev, "fw_status=0x%x\n", metadata.fw_status);
+
+	if (copy_to_user(arg, &metadata, sizeof(metadata)))
+		ret = -EFAULT;
+
+	return ret;
+}
+
+static long bcm_vk_load_image(struct bcm_vk *vk, struct vk_image *arg)
+{
+	struct device *dev = &vk->pdev->dev;
+	const struct firmware  *fw;
+	struct vk_image image;
+	long ret = 0;
+	unsigned long i;
+	uint64_t offset;
+	u32 codepush;
+	u32 ram_open;
+	int timeout_ms = 100; /* Allow minimum 100ms for timeout responses */
+
+	if (copy_from_user(&image, arg, sizeof(image))) {
+		ret = -EACCES;
+		goto err_out;
+	}
+	dev_dbg(dev, "image type 0x%x", image.type);
+
+	ret = request_firmware(&fw, image.filename, dev);
+	if (ret) {
+		dev_err(dev, "Error %ld requesting firmware file: %s\n",
+			ret, image.filename);
+		goto err_out;
+	}
+	dev_dbg(dev, "size=0x%zx\n", fw->size);
+
+	if (image.type == VK_IMAGE_TYPE_BOOT1) {
+		offset = BAR1_CODEPUSH_BASE_FASTBOOT;
+		codepush = CODEPUSH_FASTBOOT;
+		if (fw->size > SZ_256K) {
+			dev_err(dev, "Error size 0x%zx > 256K\n", fw->size);
+			ret = -EINVAL;
+			goto err_firmware_out;
+		}
+
+		ram_open = vkread32(vk, BAR_0, BAR_FB_RAM_OPEN);
+		dev_dbg(dev, "ram_open=0x%x\n", ram_open);
+
+		/* Write a 1 to request SRAM open bit */
+		vkwrite32(vk, 1, BAR_0, BAR_FB_REQ);
+
+		/* Wait for SRAM to open */
+		do {
+			ram_open = vkread32(vk, BAR_0, BAR_FB_RAM_OPEN);
+			dev_dbg(dev, "ram_open=0x%x\n", ram_open);
+			if (ram_open & RAM_OPEN_SRAM)
+				break;
+
+			/* Sleep minimum of 1ms per loop */
+			usleep_range(1000, 10000);
+			timeout_ms--;
+		} while (timeout_ms);
+	} else if (image.type == VK_IMAGE_TYPE_BOOT2) {
+		offset = BAR1_CODEPUSH_BASE_FIRMWARE;
+		codepush = CODEPUSH_FIRMWARE;
+		if (fw->size > SZ_2M) {
+			dev_err(dev, "Error size 0x%zx > 2M\n", fw->size);
+			ret = -EINVAL;
+			goto err_firmware_out;
+		}
+
+		do {
+			ram_open = vkread32(vk, BAR_0, BAR_FB_RAM_OPEN);
+			dev_dbg(dev, "ram_open=0x%x\n", ram_open);
+			if (ram_open & RAM_OPEN_DDR)
+				break;
+
+			/* Sleep minimum of 1ms per loop */
+			usleep_range(1000, 10000);
+			timeout_ms--;
+		} while (timeout_ms);
+	} else {
+		dev_err(dev, "Error invalid image type 0x%x\n", image.type);
+		ret = -EINVAL;
+		goto err_firmware_out;
+	}
+
+	if (timeout_ms <= 0) {
+		dev_err(dev, "timeout\n");
+		ret = -ETIMEDOUT;
+		goto err_firmware_out;
+	}
+
+	for (i = 0; i < fw->size; i += sizeof(u32))
+		vkwrite32(vk, fw->data[i], BAR_1, offset + i);
+
+	dev_dbg(dev, "Signaling 0x%x\n", codepush);
+	vkwrite32(vk, codepush, BAR_0, BAR_CODEPUSH_ADDRESS);
+
+err_firmware_out:
+	release_firmware(fw);
+
+err_out:
+	return ret;
+}
+
+static long bcm_vk_access_bar(struct bcm_vk *vk, struct vk_access *arg)
+{
+	struct device *dev = &vk->pdev->dev;
+	struct vk_access access;
+	long ret = 0;
+	u32 value;
+	long i;
+	long num;
+
+	if (copy_from_user(&access, arg, sizeof(struct vk_access))) {
+		ret = -EACCES;
+		goto err_out;
+	}
+	dev_dbg(dev, "barno=0x%x\n", access.barno);
+	dev_dbg(dev, "type=0x%x\n", access.type);
+	if (access.type == VK_ACCESS_READ) {
+		dev_dbg(dev, "read barno:%d offset:0x%llx len:0x%x\n",
+			access.barno, access.offset, access.len);
+		num = access.len / sizeof(u32);
+		for (i = 0; i < num; i++) {
+			value = vkread32(vk, access.barno, access.offset + i);
+			ret = put_user(value, access.data + i);
+			if (ret)
+				goto err_out;
+
+			dev_dbg(dev, "0x%x\n", value);
+		}
+	} else if (access.type == VK_ACCESS_WRITE) {
+		dev_dbg(dev, "write barno:%d offset:0x%llx len:0x%x\n",
+			access.barno, access.offset, access.len);
+		num = access.len / sizeof(u32);
+		for (i = 0; i < num; i++) {
+			ret = get_user(value, access.data + i);
+			if (ret)
+				goto err_out;
+
+			vkwrite32(vk, value, access.barno, access.offset + i);
+			dev_dbg(dev, "0x%x\n", value);
+		}
+	} else {
+		dev_dbg(dev, "error\n");
+		ret = -EINVAL;
+		goto err_out;
+	}
+err_out:
+	return ret;
+}
+
+static long bcm_vk_reset(struct bcm_vk *vk, struct vk_reset *arg)
+{
+	struct device *dev = &vk->pdev->dev;
+	struct vk_reset reset;
+	long ret = 0;
+
+	if (copy_from_user(&reset, arg, sizeof(struct vk_reset))) {
+		ret = -EACCES;
+		goto err_out;
+	}
+	dev_err(dev, "TODO: Issue Reset 0x%x, 0x%x\n", reset.arg1, reset.arg2);
+
+err_out:
+	return ret;
+}
 
 static long bcm_vk_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	long ret = -EINVAL;
+	struct bcm_vk *vk = to_bcm_vk(file->private_data);
+	void __user *argp = (void __user *)arg;
+
+	dev_dbg(&vk->pdev->dev,
+		"ioctl, cmd=0x%02x, arg=0x%02lx\n",
+		cmd, arg);
+
+	mutex_lock(&vk->mutex);
+
+	switch (cmd) {
+	case VK_IOCTL_GET_METADATA:
+		ret = bcm_vk_get_metadata(vk, argp);
+		break;
+
+	case VK_IOCTL_LOAD_IMAGE:
+		ret = bcm_vk_load_image(vk, argp);
+		break;
+
+	case VK_IOCTL_ACCESS_BAR:
+		ret = bcm_vk_access_bar(vk, argp);
+		break;
+
+	case VK_IOCTL_RESET:
+		ret = bcm_vk_reset(vk, argp);
+		break;
+
+	default:
+		break;
+	}
+
+	mutex_unlock(&vk->mutex);
+
 	return ret;
 }
 
@@ -60,6 +324,8 @@ static int bcm_vk_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 		return -ENOMEM;
 
 	vk->pdev = pdev;
+
+	mutex_init(&vk->mutex);
 
 	err = pci_enable_device(pdev);
 	if (err) {
