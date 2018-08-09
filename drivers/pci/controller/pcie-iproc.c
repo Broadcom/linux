30b1352747a11da0808ed7862172556f34b1bcc7
Ray Jui <ray.jui@broadcom.com>
2018-08-09 16:32:55 -0700

    SOC-7853 PCI: iproc: Add ACPI/ECAM support
    Add ACPI/ECAM support for PAXCv2 under the iProc PCIe driver.
    
    Smoketest:no
    Change-Id: Ib38d21bee5f88d956516e1291bb9c32b81d6cfa6
    Signed-off-by: Ray Jui <ray.jui@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/125789
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/pci/controller/pcie-iproc.c
+++ drivers/pci/controller/pcie-iproc.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2015 Broadcom Corporation
  */
 
+#include <linux/acpi.h>
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/msi.h>
@@ -15,6 +16,7 @@
 #include <linux/interrupt.h>
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/irqchip/chained_irq.h>
+#include <linux/pci-ecam.h>
 #include <linux/platform_device.h>
 #include <linux/of_address.h>
 #include <linux/of_pci.h>
@@ -412,7 +414,16 @@ static const u16 iproc_pcie_corrupt_cap_did[] = {
 
 static inline struct iproc_pcie *iproc_data(struct pci_bus *bus)
 {
-	struct iproc_pcie *pcie = bus->sysdata;
+	struct iproc_pcie *pcie;
+	struct pci_config_window *cfg;
+
+	if (acpi_disabled) {
+		pcie = bus->sysdata;
+	} else {
+		cfg = bus->sysdata;
+		pcie = cfg->priv;
+	}
+
 	return pcie;
 }
 
@@ -666,9 +677,9 @@ static void __iomem *iproc_pcie_map_cfg_bus(struct iproc_pcie *pcie,
 	return iproc_pcie_map_ep_cfg_reg(pcie, busno, slot, fn, where);
 }
 
-static void __iomem *iproc_pcie_bus_map_cfg_bus(struct pci_bus *bus,
-						unsigned int devfn,
-						int where)
+void __iomem *iproc_pcie_bus_map_cfg_bus(struct pci_bus *bus,
+					 unsigned int devfn,
+					 int where)
 {
 	return iproc_pcie_map_cfg_bus(iproc_data(bus), bus->number, devfn,
 				      where);
@@ -718,8 +729,8 @@ static int iproc_pci_raw_config_write32(struct iproc_pcie *pcie,
 	return PCIBIOS_SUCCESSFUL;
 }
 
-static int iproc_pcie_config_read32(struct pci_bus *bus, unsigned int devfn,
-				    int where, int size, u32 *val)
+int iproc_pcie_config_read32(struct pci_bus *bus, unsigned int devfn,
+			     int where, int size, u32 *val)
 {
 	int ret;
 	struct iproc_pcie *pcie = iproc_data(bus);
@@ -734,8 +745,8 @@ static int iproc_pcie_config_read32(struct pci_bus *bus, unsigned int devfn,
 	return ret;
 }
 
-static int iproc_pcie_config_write32(struct pci_bus *bus, unsigned int devfn,
-				     int where, int size, u32 val)
+int iproc_pcie_config_write32(struct pci_bus *bus, unsigned int devfn,
+			      int where, int size, u32 val)
 {
 	int ret;
 
@@ -1547,7 +1558,7 @@ static void iproc_pcie_msi_disable(struct iproc_pcie *pcie)
 	iproc_msi_exit(pcie);
 }
 
-static int iproc_pcie_rev_init(struct iproc_pcie *pcie)
+int iproc_pcie_rev_init(struct iproc_pcie *pcie)
 {
 	struct device *dev = pcie->dev;
 	unsigned int reg_idx;
