ea16a98ebb29869e13c278ef5286b802019dad6d
Srinath Mannam <srinath.mannam@broadcom.com>
2018-12-06 16:11:04 +0530

    SOC-8474: Add dma-resv window list
    Add a dma_resv parameter in pci host bridge structure
    to hold resource entries list of memory regions for
    which IOVAs has to reserve.
    
    IOMMU framework reserve IOVA for this list of address
    range while initializing IOMMU domain of corresponding
    PCI EP connected to the HOST.
    
    PCIe host driver will add resource entries to this
    list based on requirements.
    Few hosts can't use all inbound address, so those
    address regions will be add to this list to avoid
    IOMMU mapping.
    
    Change-Id: Ib2b872a0ab578be0e81dbfa6dd74e4a01d8062cc
    Signed-off-by: Srinath Mannam <srinath.mannam@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/76019
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Vikram Prakash <vikram.prakash@broadcom.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -544,6 +544,7 @@ struct pci_host_bridge *pci_alloc_host_bridge(size_t priv)
 		return NULL;
 
 	INIT_LIST_HEAD(&bridge->windows);
+	INIT_LIST_HEAD(&bridge->dma_resv);
 	bridge->dev.release = pci_release_host_bridge_dev;
 
 	/*
@@ -572,6 +573,7 @@ struct pci_host_bridge *devm_pci_alloc_host_bridge(struct device *dev,
 		return NULL;
 
 	INIT_LIST_HEAD(&bridge->windows);
+	INIT_LIST_HEAD(&bridge->dma_resv);
 	bridge->dev.release = devm_pci_release_host_bridge_dev;
 
 	return bridge;
@@ -581,6 +583,7 @@ EXPORT_SYMBOL(devm_pci_alloc_host_bridge);
 void pci_free_host_bridge(struct pci_host_bridge *bridge)
 {
 	pci_free_resource_list(&bridge->windows);
+	pci_free_resource_list(&bridge->dma_resv);
 
 	kfree(bridge);
 }
