0fae2e51f41c13890edae0370516a7b98219a463
Scott Branden <scott.branden@broadcom.com>
2019-08-08 10:56:25 -0700

    Revert "VOIP-7106: ASoC: omega-wa: Add machine driver for wireless audio board"
    This reverts commit aa2e05ae6639bb5b866887fe09b5928b6dffc775.
    
    Revert "VOIP-7106: ASoC: omega-wa: Add control of ak4458 reset"
    
    This reverts commit f64760de65536cd9097ca144204249c8fcaa2a57.
    
    Revert "VOIP-7610: ASoc: Omega_wa: Add amixer control for tweaking clock"
    
    This reverts commit 6fc4bf761866b8cf18958422e149064536cfd5af.
    
    Revert "VOIP-7887: ASoC: omega_wa: Add missing tdm config for audioh link"
    
    This reverts commit 647bad7afcb7b042ad2180fb35d9e49dc0ce7638.
    
    Revert "VOIP-8111: ASoC: omega-wa: Initialize the pll tweaking code"
    
    This reverts commit 3f929963206d3b0206923303324e80a274f4196a.
    
    Revert "VOIP-7909; ASoC: audioH: Add example of how to use M7 interface"
    
    This reverts commit fdcb2af20d7c496e3208cb90df00adfe8c53a992.
    
    Change-Id: I45ff53187f0d5566afd261ba2dec963ffabc05cb
    
    Revert "VOIP-8111: ASoC: omega: Fix audio PLL adjustment"
    
    This reverts commit e8c9d231c415a2a34307fa34ea7d69d17b093c7d.
    
    Revert "VOIP-8186: ASoC: omega: Stop using regmap_read_poll_timeout"
    
    This reverts commit 8cacb3158d18980529842a7620f0df6ee8f0caf0.
    
    Revert "VOIP-8165: ASoC: omega: add suspend resume support"
    
    This reverts commit d469c1aa85a73568b34ab5e74cafb5367fa2c048.
    
    Revert "VOIP-8110: ASoC: omega-ssp: Add names to regmap configs"
    
    This reverts commit 80318b6cb8f6b2bff0aa9117402436bd69a7776c.
    
    Revert "VOIP-7106: ASoC: omega: convert to SPDX headers"
    
    This reverts commit da22ff01035c76799c3a489bf8c72c1da2e9b488.
    
    Revert "VOIP-7909: ASoC: omega: Add support of the m7 audio port"
    
    This reverts commit 0e768c58e2c405e31bb4072b359e02eb9ce305d9.
    
    Revert "VOIP-7106: ASoC: omega-svk: Add optional gpio for headset amp control"
    
    This reverts commit c3b407370a9e25286009ad89824cbe7392aee1c5.
    
    Revert "VOIP-7016: Create ASoC machine driver for Omega SVK"
    
    This reverts commit 66b3dd3d893912b024f66d5203b6f1546dcf12f0.
    
    Change-Id: If2470834b20faafb7ed4eae4ad263c22480b23b4
    
    Revert "VOIP-7106: ASoC: bcm: Add support for the Omega SoC"
    
    This reverts commit 35657072dff72d97195befca38f051670e48bb11.
    
    Change-Id: I0bca0962beacf09b66fa848c85ae58b8468eeee1
    
    Revert "VOIP-7830: ASoC: bcm91140x15-entphn: Add missing gpio for amp enable"
    
    This reverts commit ce35419cd8a2ddaff04ffba50e037a0052d5f3b5.
    
    Revert "VOIP-7106: ASoC: Omega enterprise phone needs tdm configured"
    
    This reverts commit 05d66bd6de04b655f6bc9df65021fe93356ec59e.
    
    Revert "VOIP-7106: ASoC: Create machine driver for bcm91140x15 voip phone"
    
    This reverts commit 4622ccae0c64f30cd8f5cab859dc16193f829773.
    
    No need to support in bcm/master.
    
    Change-Id: I019689a260953533df8837beb8d88ea37071d293
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/98471
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- sound/soc/bcm/omega-ssp.h
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright (C) 2018 Broadcom
- */#ifndef __OMEGA_SSP_H__
-#define __OMEGA_SSP_H__
-
-int omega_ssp_set_custom_fsync_width(struct snd_soc_dai *cpu_dai, int len);
-
-#endif
