1436b481a750fd7678dccee037c7ac4639fd1898
Scott Branden <scott.branden@broadcom.com>
2019-08-16 14:04:01 -0700

    Revert "VOIP-6474: ARM: bcm: Add support for power management on cygnus"
    Sync with upstream version.  Changes not needed in bcm/master.
    
    Revert "VOIP-7132 pm: invert order of L1 and L2 flush in sleep entry sequence"
    
    Not need to support in bcm/master.
    
    This reverts commit c6d95998f5784db374e5d250814b1b9713236fa9.
    
    Change-Id: I763c77e8b031f19ec364ba2d05160a7e5222cfbc
    
    Revert "VOIP-6948 : Change error message to something less severe"
    
    This reverts commit a484260f16b1acc72690f34c4d7e8a5e2051b4cd.
    
    Revert "VOIP-6911: ARM: bcm: Add trusted mode support to power management"
    
    This reverts commit c07b4f825fb6770cd06eea267a4db4cd8de181cc.
    
    Change-Id: Ie2f7187425aa2336f8bb21efa4e7ea227e6076b0
    
    Revert "VOIP-6878: Add support for building Cygnus in secure mode."
    
    This reverts commit 48459c929bef1becb52c259c21c807e0186c2aa7.
    
    Revert "VOIP-6635: Enable the L2C in the Cygnus platform init"
    
    This reverts commit 154548eeb1c537a2c2036dd1e3ecc54ea96f8724.
    
    Revert "VOIP-6474: ARM: bcm: Add support for power management on cygnus"
    
    This reverts commit 5827ef54ab503d6de85c87a728a506027c541617.
    
    Revert "VOIP-5227: Add dummy iProc suspend hooks"
    
    This reverts commit a1b6ac5e6170f65b6c7d4df98e6649bd781114d0.
    
    Change-Id: Ia1d8b85aa3f1d072947c06d106bab0db307cb9ba
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/100319
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- arch/arm/mach-bcm/bcm_cygnus.c
+++ arch/arm/mach-bcm/bcm_cygnus.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2017 Broadcom
+ * Copyright (C) 2014 Broadcom Corporation
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -10,32 +10,15 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+
 #include <asm/mach/arch.h>
-#include <asm/mach/bcm_optee_smc.h>
-#include <asm/mach/map.h>
-#include <asm/proc-fns.h>
-#include "cygnus_pm.h"
 
 static const char * const bcm_cygnus_dt_compat[] __initconst = {
 	"brcm,cygnus",
 	NULL,
 };
 
-static void cygnus_early_init(void)
-{
-#ifdef CONFIG_OPTEE
-	struct arm_smccc_res res;
-	unsigned int cmd;
-
-	cmd = FAST_SMC(SSAPI_ENABLE_L2_CACHE);
-	arm_smccc_smc(cmd, machine_desc->l2c_aux_val,
-		      machine_desc->l2c_aux_mask, 0, 0, 0, 0, 0, &res);
-#endif
-}
-
 DT_MACHINE_START(BCM_CYGNUS_DT, "Broadcom Cygnus SoC")
-	.init_early	= cygnus_early_init,
-	.init_late	= cygnus_pm_init,
 	.l2c_aux_val	= 0,
 	.l2c_aux_mask	= ~0,
 	.dt_compat = bcm_cygnus_dt_compat,
