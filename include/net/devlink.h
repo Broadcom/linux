fd371b2419de12d133461463cd45501fe16e3061
Vikas Gupta <vikas.gupta@broadcom.com>
2019-11-11 12:13:11 +0530

    SOC-10449: devlink: add support for reporter recovery completion
    It is possible that a reporter recovery completion do not finish
    successfully when recovery is triggered via
    devlink_health_reporter_recover as recovery could be processed in
    different context. In such scenario an error is returned by driver when
    recover hook is invoked and successful recovery completion is
    intimated later.
    Expose devlink recover done API to update recovery stats.
    
    Change-Id: I3c6f8053d56e9b413becf709efcd8049c3d29568
    Signed-off-by: Vikas Gupta <vikas.gupta@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/106796
    Tested-by: vpx_autobuild status <vpx_autobuild@broadcom.com>
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Vasundhara Volam <vasundhara-v.volam@broadcom.com>
    Reviewed-by: Ray Jui <ray.jui@broadcom.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -723,6 +723,8 @@ int devlink_health_report(struct devlink_health_reporter *reporter,
 void
 devlink_health_reporter_state_update(struct devlink_health_reporter *reporter,
 				     enum devlink_health_reporter_state state);
+void
+devlink_health_reporter_recovery_done(struct devlink_health_reporter *reporter);
 
 void devlink_compat_running_version(struct net_device *dev,
 				    char *buf, size_t len);
