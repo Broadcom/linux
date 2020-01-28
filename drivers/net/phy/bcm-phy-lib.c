3fc347a8e13b2667846773f0cc38aaef44376665
Scott Branden <scott.branden@broadcom.com>
2020-01-28 15:10:48 -0800

    Revert "SOC-6393: net: phy: Add BCM54210S initialization sequence"
    Revert and regenerate against v5.5 kernel.
    
    This reverts commit 74e5130c3b8bdcb0e62aa7757522e6a026ff3666.
    
    Change-Id: I8048b28f2bd7c9cf6c13a73f93a2a1ae3b35719c
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/111914
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/net/phy/bcm-phy-lib.c
+++ drivers/net/phy/bcm-phy-lib.c
@@ -108,32 +108,6 @@ int bcm_phy_read_misc(struct phy_device *phydev,
 }
 EXPORT_SYMBOL_GPL(bcm_phy_read_misc);
 
-int bcm_phy_write_rdb(struct phy_device *phydev, u16 reg, u16 val)
-{
-	int rc;
-
-	rc = phy_write(phydev, BCM_542XX_RDB_ADDR,
-		       reg & BCM_542XX_RDB_ADDR_MASK);
-	if (rc  < 0)
-		return rc;
-
-	return phy_write(phydev, BCM_542XX_RDB_DATA, val);
-}
-EXPORT_SYMBOL_GPL(bcm_phy_write_rdb);
-
-int bcm_phy_read_rdb(struct phy_device *phydev, u16 reg)
-{
-	int rc;
-
-	rc = phy_write(phydev, BCM_542XX_RDB_ADDR,
-		       reg & BCM_542XX_RDB_ADDR_MASK);
-	if (rc  < 0)
-		return rc;
-
-	return phy_read(phydev, BCM_542XX_RDB_DATA);
-}
-EXPORT_SYMBOL_GPL(bcm_phy_read_rdb);
-
 int bcm_phy_ack_intr(struct phy_device *phydev)
 {
 	int reg;
@@ -163,19 +137,6 @@ int bcm_phy_config_intr(struct phy_device *phydev)
 	return phy_write(phydev, MII_BCM54XX_ECR, reg);
 }
 EXPORT_SYMBOL_GPL(bcm_phy_config_intr);
-
-int bcm_phy_power_down(struct phy_device *phydev, bool down)
-{
-	u16 val;
-
-	val = phy_read(phydev, MII_BMCR);
-	if (down)
-		val |= BCM_542XX_POWER_DOWN;
-	else
-		val &= ~BCM_542XX_POWER_DOWN;
-	return phy_write(phydev, MII_BMCR, val);
-}
-EXPORT_SYMBOL_GPL(bcm_phy_power_down);
 
 int bcm_phy_read_shadow(struct phy_device *phydev, u16 shadow)
 {
