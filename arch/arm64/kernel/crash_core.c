3c4aa3a2f6de4c3ff4aaa52c6a6224ce272e2847
Scott Branden <scott.branden@broadcom.com>
2020-08-07 10:53:39 -0700

    Revert "SOC-11028: arm64/crash_core: Export TCR_EL1.T1SZ in vmcoreinfo"
    Pickup 5.9 upstream version of patch instead.
    
    This reverts commit 7964a7f2111fb07cdcc488118523a49795ffccc3.
    
    Change-Id: Ifb0bc405a6aea21c3e9c13383c6ef38e211035d5
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/128551
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Tested-by: Scott Branden <scott.branden@broadcom.com>

--- arch/arm64/kernel/crash_core.c
+++ arch/arm64/kernel/crash_core.c
@@ -8,13 +8,6 @@
 #include <asm/cpufeature.h>
 #include <asm/memory.h>
 
-static inline u64 get_tcr_el1_t1sz(void);
-
-static inline u64 get_tcr_el1_t1sz(void)
-{
-	return (read_sysreg(tcr_el1) & TCR_T1SZ_MASK) >> TCR_T1SZ_OFFSET;
-}
-
 void arch_crash_save_vmcoreinfo(void)
 {
 	VMCOREINFO_NUMBER(VA_BITS);
@@ -23,8 +16,6 @@ void arch_crash_save_vmcoreinfo(void)
 						kimage_voffset);
 	vmcoreinfo_append_str("NUMBER(PHYS_OFFSET)=0x%llx\n",
 						PHYS_OFFSET);
-	vmcoreinfo_append_str("NUMBER(tcr_el1_t1sz)=0x%llx\n",
-						get_tcr_el1_t1sz());
 	vmcoreinfo_append_str("KERNELOFFSET=%lx\n", kaslr_offset());
 	vmcoreinfo_append_str("NUMBER(KERNELPACMASK)=0x%llx\n",
 						system_supports_address_auth() ?
