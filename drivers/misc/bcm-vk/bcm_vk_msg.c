ed4d3bea2258aa8ea981bbd8026c9e8c51e5560b
Scott Branden <scott.branden@broadcom.com>
2020-10-02 10:01:14 -0700

    SOC-10289 misc: bcm-vk: change QSTATS print from dev_info to dev_dbg
    Change QSTATS print from dev_info to dev_dbg as upstream doesn't like
    noisy drivers.
    
    Change-Id: Ia7f836441a5d46af54b072df2d9b9a26bbeb7e40
    Reviewed-on: https://gerrit-ccxsw.broadcom.net/132611
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Desmond Yan <desmond.yan@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- drivers/misc/bcm-vk/bcm_vk_msg.c
+++ drivers/misc/bcm-vk/bcm_vk_msg.c
@@ -114,13 +114,13 @@ static void bcm_vk_update_qstats(struct bcm_vk *vk,
 	qcnts->acc_sum += occupancy;
 	if (++qcnts->cnt >= BCM_VK_QSTATS_ACC_CNT) {
 		/* log average and clear counters */
-		dev_info(&vk->pdev->dev,
-			 "%s[%d]: Max: [%3d/%3d] Acc %d num %d, Aver %d\n",
-			 tag, qstats->q_num,
-			 qcnts->max_occ, qcnts->max_abs,
-			 qcnts->acc_sum,
-			 qcnts->cnt,
-			 qcnts->acc_sum / qcnts->cnt);
+		dev_dbg(&vk->pdev->dev,
+			"%s[%d]: Max: [%3d/%3d] Acc %d num %d, Aver %d\n",
+			tag, qstats->q_num,
+			qcnts->max_occ, qcnts->max_abs,
+			qcnts->acc_sum,
+			qcnts->cnt,
+			qcnts->acc_sum / qcnts->cnt);
 
 		qcnts->cnt = 0;
 		qcnts->max_occ = 0;
