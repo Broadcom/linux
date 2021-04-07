54da2f6dfd429135c5c0368dfb8ba2c7a9da8c28
Scott Branden <scott.branden@broadcom.com>
2021-04-07 16:58:28 -0700

    Revert "SOC-9024 thermal: Add new API: thermal_zone_get_crit_temp"
    This reverts commit c3026e4b56a4f6f1349aff10d8729bcdd3799eec.
    
    Change-Id: Ib1a677a3193572398144ab7e6aa928111bf353ea
    Reviewed-on: https://gerrit-ccxsw.broadcom.net/144514
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: CCXSW Coverity <ccxswcoverity@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -396,7 +396,6 @@ struct thermal_zone_device *thermal_zone_get_zone_by_name(const char *name);
 int thermal_zone_get_temp(struct thermal_zone_device *tz, int *temp);
 int thermal_zone_get_slope(struct thermal_zone_device *tz);
 int thermal_zone_get_offset(struct thermal_zone_device *tz);
-int thermal_zone_get_crit_temp(struct thermal_zone_device *tz, int *temp);
 
 void thermal_cdev_update(struct thermal_cooling_device *);
 void thermal_notify_framework(struct thermal_zone_device *, int);
@@ -443,9 +442,6 @@ static inline int thermal_zone_get_slope(
 { return -ENODEV; }
 static inline int thermal_zone_get_offset(
 		struct thermal_zone_device *tz)
-{ return -ENODEV; }
-static inline int thermal_zone_get_crit_temp
-		(struct thermal_zone_device *tz, int *temp)
 { return -ENODEV; }
 
 static inline void thermal_cdev_update(struct thermal_cooling_device *cdev)
